73286787bfbc7930b731bcc48f5b84384849ccb7
Will Daly <will.e.daly@gmail.com>
2020-07-25 13:11:37 -0700

    Implement mutators for deletion
    CompositeMutator executes a sequence of mutators, which is necessary
    to express "delete a character then move the cursor onto a line if necessary".
    
    DeleteMutator deletes characters from the cursor up to (but not including)
    a position returned by a locator.  This can be used to implement any
    deletion (to next character in line, end of line, end of file, previous word, etc)
    by plugging in a different locator.

--- internal/pkg/exec/mutator.go
+++ internal/pkg/exec/mutator.go
@@ -1,11 +1,40 @@
 package exec
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/wedaly/aretext/internal/pkg/text"
+)
 
 // Mutator modifies the state of the cursor or text.
 type Mutator interface {
 	fmt.Stringer
 	Mutate(state *State)
+}
+
+// CompositeMutator executes a series of mutations.
+type CompositeMutator struct {
+	subMutators []Mutator
+}
+
+func NewCompositeMutator(subMutators []Mutator) Mutator {
+	return &CompositeMutator{subMutators}
+}
+
+// Mutate executes a series of mutations in order.
+func (cm *CompositeMutator) Mutate(state *State) {
+	for _, mut := range cm.subMutators {
+		mut.Mutate(state)
+	}
+}
+
+func (cm *CompositeMutator) String() string {
+	args := make([]string, 0, len(cm.subMutators))
+	for _, mut := range cm.subMutators {
+		args = append(args, mut.String())
+	}
+	return fmt.Sprintf("Composite(%s)", strings.Join(args, ","))
 }
 
 // cursorMutator updates the current location of the cursor.
@@ -48,4 +77,40 @@ func (irm *insertRuneMutator) Mutate(state *State) {
 
 func (irm *insertRuneMutator) String() string {
 	return fmt.Sprintf("InsertRune(%c)", irm.r)
+}
+
+// deleteMutator deletes characters from the cursor up to a location.
+type deleteMutator struct {
+	loc Locator
+}
+
+func NewDeleteMutator(loc Locator) Mutator {
+	return &deleteMutator{loc}
+}
+
+// Mutate deletes characters from the cursor position up to (but not including) the position returned by the locator.
+// It can delete either forwards or backwards from the cursor.
+// The cursor position will be set to the start of the deleted region,
+// which could be on a newline character or past the end of the text.
+func (dm *deleteMutator) Mutate(state *State) {
+	startPos := state.cursor.position
+	deleteToPos := dm.loc.Locate(state).position
+
+	if startPos < deleteToPos {
+		dm.deleteCharacters(state.tree, startPos, deleteToPos-startPos)
+		state.cursor = cursorState{position: startPos}
+	} else if startPos > deleteToPos {
+		dm.deleteCharacters(state.tree, deleteToPos, startPos-deleteToPos)
+		state.cursor = cursorState{position: deleteToPos}
+	}
+}
+
+func (dm *deleteMutator) deleteCharacters(tree *text.Tree, pos uint64, count uint64) {
+	for i := uint64(0); i < count; i++ {
+		tree.DeleteAtPosition(pos)
+	}
+}
+
+func (dm *deleteMutator) String() string {
+	return fmt.Sprintf("Delete(%s)", dm.loc)
 }
---
+++ internal/pkg/exec/mutator_test.go
@@ -0,0 +1,121 @@
+package exec
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/wedaly/aretext/internal/pkg/text"
+)
+
+func allTextFromTree(t *testing.T, tree *text.Tree) string {
+	reader := tree.ReaderAtPosition(0, text.ReadDirectionForward)
+	retrievedBytes, err := ioutil.ReadAll(reader)
+	require.NoError(t, err)
+	return string(retrievedBytes)
+}
+
+func TestInsertRuneMutator(t *testing.T) {
+	testCases := []struct {
+		name           string
+		inputString    string
+		initialCursor  cursorState
+		insertRune     rune
+		expectedCursor cursorState
+		expectedText   string
+	}{
+		{
+			name:           "insert into empty string",
+			inputString:    "",
+			initialCursor:  cursorState{position: 0},
+			insertRune:     'x',
+			expectedCursor: cursorState{position: 1},
+			expectedText:   "x",
+		},
+		{
+			name:           "insert in middle of string",
+			inputString:    "abcd",
+			initialCursor:  cursorState{position: 1},
+			insertRune:     'x',
+			expectedCursor: cursorState{position: 2},
+			expectedText:   "axbcd",
+		},
+		{
+			name:           "insert at end of string",
+			inputString:    "abcd",
+			initialCursor:  cursorState{position: 4},
+			insertRune:     'x',
+			expectedCursor: cursorState{position: 5},
+			expectedText:   "abcdx",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tree, err := text.NewTreeFromString(tc.inputString)
+			require.NoError(t, err)
+			state := State{tree, tc.initialCursor}
+			mutator := NewInsertRuneMutator(tc.insertRune)
+			mutator.Mutate(&state)
+			assert.Equal(t, tc.expectedCursor, state.cursor)
+			assert.Equal(t, tc.expectedText, allTextFromTree(t, state.tree))
+		})
+	}
+}
+
+func TestDeleteMutator(t *testing.T) {
+	testCases := []struct {
+		name           string
+		inputString    string
+		initialCursor  cursorState
+		locator        Locator
+		expectedCursor cursorState
+		expectedText   string
+	}{
+		{
+			name:           "delete from empty string",
+			inputString:    "",
+			initialCursor:  cursorState{position: 0},
+			locator:        NewCharInLineLocator(text.ReadDirectionForward, 1, true),
+			expectedCursor: cursorState{position: 0},
+			expectedText:   "",
+		},
+		{
+			name:           "delete next character at start of string",
+			inputString:    "abcd",
+			initialCursor:  cursorState{position: 0},
+			locator:        NewCharInLineLocator(text.ReadDirectionForward, 1, true),
+			expectedCursor: cursorState{position: 0},
+			expectedText:   "bcd",
+		},
+		{
+			name:           "delete from end of text",
+			inputString:    "abcd",
+			initialCursor:  cursorState{position: 3},
+			locator:        NewCharInLineLocator(text.ReadDirectionForward, 1, true),
+			expectedCursor: cursorState{position: 3},
+			expectedText:   "abc",
+		},
+		{
+			name:           "delete multiple characters",
+			inputString:    "abcd",
+			initialCursor:  cursorState{position: 1},
+			locator:        NewCharInLineLocator(text.ReadDirectionForward, 10, true),
+			expectedCursor: cursorState{position: 1},
+			expectedText:   "a",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tree, err := text.NewTreeFromString(tc.inputString)
+			require.NoError(t, err)
+			state := State{tree, tc.initialCursor}
+			mutator := NewDeleteMutator(tc.locator)
+			mutator.Mutate(&state)
+			assert.Equal(t, tc.expectedCursor, state.cursor)
+			assert.Equal(t, tc.expectedText, allTextFromTree(t, state.tree))
+		})
+	}
+}

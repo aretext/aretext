7ce3c4baa211e23714df48c9ccad67deac18e1f3
Will Daly <will.e.daly@gmail.com>
2020-11-01 09:14:30 -0800

    Implement batch tokenizer

---
+++ internal/pkg/syntax/parser/tokenizer.go
@@ -0,0 +1,120 @@
+package parser
+
+import (
+	"io"
+
+	"github.com/pkg/errors"
+	"github.com/wedaly/aretext/internal/pkg/text/utf8"
+)
+
+// TokenRole represents the role a token plays.
+// This is mainly used for syntax highlighting.
+type TokenRole int
+
+const (
+	TokenRoleNone = TokenRole(iota)
+	TokenRoleOperator
+	TokenRoleKeyword
+	TokenRoleIdentifier
+	TokenRoleNumber
+	TokenRoleString
+	TokenRoleComment
+)
+
+// Token represents a distinct element in a larger text.
+type Token struct {
+	StartPos uint64
+	EndPos   uint64
+	Role     TokenRole
+}
+
+// TokenizerRule represents a rule for parsing a particular token.
+type TokenizerRule struct {
+	Regexp    string
+	TokenRole TokenRole
+}
+
+// Tokenizer parses a text into tokens based on a set of rules.
+type Tokenizer struct {
+	StateMachine *Dfa
+	Rules        []TokenizerRule
+}
+
+// GenerateTokenizer compiles a tokenizer from a set of rules.
+func GenerateTokenizer(rules []TokenizerRule) (*Tokenizer, error) {
+	nfa := EmptyLanguageNfa()
+	for i, r := range rules {
+		regexp, err := ParseRegexp(r.Regexp)
+		if err != nil {
+			return nil, errors.Wrapf(err, "parse rule regexp")
+		}
+		ruleNfa := regexp.CompileNfa().SetAcceptAction(i)
+		nfa = nfa.Union(ruleNfa)
+	}
+
+	tokenizer := &Tokenizer{
+		StateMachine: nfa.CompileDfa(),
+		Rules:        rules,
+	}
+
+	return tokenizer, nil
+}
+
+// TokenizeAll splits the entire input text into tokens.
+// The input text MUST be valid UTF-8.
+func (t *Tokenizer) TokenizeAll(r io.ReadSeeker, textLen uint64) ([]Token, error) {
+	var tokens []Token
+	pos := uint64(0)
+	for pos < textLen {
+		accepted, endPos, actions, err := t.StateMachine.MatchLongest(r, pos, textLen)
+		if err != nil {
+			return nil, errors.Wrapf(err, "tokenizing input")
+		}
+
+		// Identify a token if the DFA accepts AND the accepted prefix is non-empty.
+		// (An empty prefix can occur when "a*" matches "" at the beginning of "bcd")
+		if accepted && endPos > pos {
+			rule := t.actionsToRule(actions) // Choose matched rule with the lowest index.
+			tokens = append(tokens, Token{
+				StartPos: pos,
+				EndPos:   endPos,
+				Role:     rule.TokenRole,
+			})
+			pos = endPos
+		} else {
+			// If we couldn't find a match, advance to the next position and try again.
+			pos++
+			if err := advanceReaderOneRune(r); err != nil {
+				return nil, errors.Wrapf(err, "advance reader")
+			}
+		}
+	}
+	return tokens, nil
+}
+
+func (t *Tokenizer) actionsToRule(actions []int) TokenizerRule {
+	ruleIdx := actions[0]
+	for _, a := range actions[1:] {
+		if a < ruleIdx {
+			ruleIdx = a
+		}
+	}
+	return t.Rules[ruleIdx]
+}
+
+func advanceReaderOneRune(r io.ReadSeeker) error {
+	var buf [1]byte
+
+	if _, err := r.Read(buf[:1]); err != nil && err != io.EOF {
+		return errors.Wrapf(err, "read first byte")
+	}
+
+	w := int64(utf8.CharWidth[buf[0]])
+	if w > 1 {
+		if _, err := r.Seek(w-1, io.SeekCurrent); err != nil {
+			return errors.Wrapf(err, "seek forward")
+		}
+	}
+
+	return nil
+}
---
+++ internal/pkg/syntax/parser/tokenizer_test.go
@@ -0,0 +1,84 @@
+package parser
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTokenizeAll(t *testing.T) {
+	tokenizer, err := GenerateTokenizer([]TokenizerRule{
+		{
+			Regexp:    `\+`,
+			TokenRole: TokenRoleOperator,
+		},
+		{
+			Regexp:    "def|return",
+			TokenRole: TokenRoleKeyword,
+		},
+		{
+			Regexp:    "[a-zA-Z][a-zA-Z0-9]+",
+			TokenRole: TokenRoleIdentifier,
+		},
+		{
+			Regexp:    "[0-9]+",
+			TokenRole: TokenRoleNumber,
+		},
+	})
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name           string
+		inputText      string
+		expectedTokens []Token
+	}{
+		{
+			name:           "empty string",
+			inputText:      "",
+			expectedTokens: nil,
+		},
+		{
+			name:      "multiple tokens",
+			inputText: "def foo():\n    return bar + 10",
+			expectedTokens: []Token{
+				// Whitespace and punctuation are skipped because they don't match any rules.
+				// "def"
+				{StartPos: 0, EndPos: 3, Role: TokenRoleKeyword},
+
+				// "foo"
+				{StartPos: 4, EndPos: 7, Role: TokenRoleIdentifier},
+
+				// "return"
+				{StartPos: 15, EndPos: 21, Role: TokenRoleKeyword},
+
+				// "bar"
+				{StartPos: 22, EndPos: 25, Role: TokenRoleIdentifier},
+
+				// "+"
+				{StartPos: 26, EndPos: 27, Role: TokenRoleOperator},
+
+				// "10"
+				{StartPos: 28, EndPos: 30, Role: TokenRoleNumber},
+			},
+		},
+		{
+			name:      "identifier with keyword prefix",
+			inputText: "defIdentifier",
+			expectedTokens: []Token{
+				{StartPos: 0, EndPos: 13, Role: TokenRoleIdentifier},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			textLen := uint64(len(tc.inputText))
+			r := strings.NewReader(tc.inputText)
+			tokens, err := tokenizer.TokenizeAll(r, textLen)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expectedTokens, tokens)
+		})
+	}
+}

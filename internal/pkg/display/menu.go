5161d67a8d00ac69dbe40a3ee5085a9f3deb7b43
Will Daly <will.e.daly@gmail.com>
2020-12-31 07:16:59 -0800

    Menu leaves one line empty for status bar

--- internal/pkg/display/menu.go
+++ internal/pkg/display/menu.go
@@ -16,6 +16,9 @@ func DrawMenu(screen tcell.Screen, menu *exec.MenuState) {
 		return
 	}
 
+	// Leave one line at the bottom for the status bar.
+	height := screenHeight - 1
+
 	// Search input
 	row := 0
 	searchInputRegion := NewScreenRegion(screen, 0, row, screenWidth, 1)
@@ -24,8 +27,8 @@ func DrawMenu(screen tcell.Screen, menu *exec.MenuState) {
 
 	// Filtered menu items (search results)
 	items, selectedIdx := menu.SearchResults()
-	items, selectedIdx = filterForVisibleItems(items, selectedIdx, screenHeight)
-	for i := 0; i < len(items) && row < screenHeight; i++ {
+	items, selectedIdx = filterForVisibleItems(items, selectedIdx, height)
+	for i := 0; i < len(items) && row < height; i++ {
 		menuItemRegion := NewScreenRegion(screen, 0, row, screenWidth, 1)
 		isSelected := i == selectedIdx
 		drawMenuItem(menuItemRegion, items[i], isSelected)
@@ -33,16 +36,16 @@ func DrawMenu(screen tcell.Screen, menu *exec.MenuState) {
 	}
 
 	// Bottom border
-	if row < screenHeight {
+	if row < height {
 		borderRegion := NewScreenRegion(screen, 0, row, screenWidth, 1)
 		borderRegion.Fill(tcell.RuneHLine, tcell.StyleDefault.Dim(true))
 		row++
 	}
 }
 
-func filterForVisibleItems(items []exec.MenuItem, selectedIdx int, screenHeight int) ([]exec.MenuItem, int) {
+func filterForVisibleItems(items []exec.MenuItem, selectedIdx int, height int) ([]exec.MenuItem, int) {
 	offset := 0
-	limit := maxNumVisibleItems(len(items), screenHeight)
+	limit := maxNumVisibleItems(len(items), height)
 	if limit == 0 {
 		return nil, 0
 	}
@@ -54,8 +57,8 @@ func filterForVisibleItems(items []exec.MenuItem, selectedIdx int, screenHeight 
 	return items[offset : offset+limit], selectedIdx
 }
 
-func maxNumVisibleItems(numItems int, screenHeight int) int {
-	maxVisible := screenHeight - 1 // Leave one line above for the search bar.
+func maxNumVisibleItems(numItems int, height int) int {
+	maxVisible := height - 1 // Leave one line above for the search bar.
 	if maxVisible < 0 {
 		maxVisible = 0
 	}

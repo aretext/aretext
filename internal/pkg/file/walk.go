bc6b8b70f50106cc07e9d979211192d68ac69346
Will Daly <will.e.daly@gmail.com>
2020-12-31 20:23:35 -0800

    Reduce number of syscalls in file.Walk
    filepath.Walk calls stat for every file, but we don't need
    all the extra info from the stat call.  It's faster to
    retrieve the file types using os.Readdir.
    
    See https://github.com/golang/go/issues/16399
    
    That issue points to a faster implementation used by go tools
    that's multi-threaded.  That may be something to explore
    in the future, but it will make the code more complicated
    so I'm avoiding it for now.

--- internal/pkg/file/walk.go
+++ internal/pkg/file/walk.go
@@ -9,21 +9,45 @@ import (
 )
 
 // Walk walks the file tree at root, evaluating the function for every file path.
+// Symlinks are skipped.
 // The file paths are absolute.
-func Walk(root string, f func(path string)) {
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+func Walk(root string, walkFn func(path string)) {
+	stack := []string{root}
+	var dirPath string
+	for len(stack) > 0 {
+		dirPath, stack = stack[len(stack)-1], stack[:len(stack)-1]
+		subdirPaths, err := processDir(dirPath, walkFn)
 		if err != nil {
-			log.Printf("Error at path '%s': %v\n", path, err)
-			return err
+			log.Printf("Error processing directory at '%s': %v\n", dirPath, err)
+			continue
 		}
-
-		if info.Mode().IsRegular() {
-			f(path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Printf("Error in file walk: %v\n", errors.Wrapf(err, "filepath.Walk"))
+		stack = append(stack, subdirPaths...)
 	}
+}
+
+func processDir(dirPath string, walkFn func(path string)) ([]string, error) {
+	f, err := os.Open(dirPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "os.Open")
+	}
+	defer f.Close()
+
+	// This is faster than calling LStat on every file, which is what filepath.Walk does.
+	// See discussion here: https://github.com/golang/go/issues/16399
+	fileInfos, err := f.Readdir(-1)
+	if err != nil {
+		return nil, errors.Wrapf(err, "File.Readdir")
+	}
+
+	subdirPaths := make([]string, 0)
+	for _, fi := range fileInfos {
+		absPath := filepath.Join(dirPath, fi.Name())
+		if fi.Mode().IsRegular() {
+			walkFn(absPath)
+		} else if fi.Mode().IsDir() {
+			subdirPaths = append(subdirPaths, absPath)
+		}
+	}
+
+	return subdirPaths, nil
 }

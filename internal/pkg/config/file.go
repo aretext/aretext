09f2c5974642704b52ee256af61de3193c31a23a
Will Daly <will.e.daly@gmail.com>
2021-01-16 12:14:28 -0800

    Add helpers to read/write a configuration file as JSON

---
+++ internal/pkg/config/file.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// LoadRuleSet loads configuration rules from a file.
+func LoadRuleSet(path string) (RuleSet, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		// Return the error directly so callers can use os.IsNotExist(err) to check if the file exists.
+		return RuleSet{}, err
+	}
+
+	var rules []Rule
+	err = json.Unmarshal(data, &rules)
+	if err != nil {
+		return RuleSet{}, errors.Wrapf(err, "json.Unmarshal")
+	}
+
+	return RuleSet{rules}, nil
+}
+
+// SaveRuleSet saves configuration rules to a file.
+func SaveRuleSet(path string, rs RuleSet) error {
+	data, err := json.MarshalIndent(rs.Rules, "", "  ")
+	if err != nil {
+		return errors.Wrapf(err, "json.Marshal")
+	}
+
+	dirPath := filepath.Dir(path)
+	err = os.MkdirAll(dirPath, 0644)
+	if err != nil {
+		return errors.Wrapf(err, "os.MkdirAll")
+	}
+
+	err = ioutil.WriteFile(path, data, 0644)
+	if err != nil {
+		return errors.Wrapf(err, "ioutil.WriteFile")
+	}
+
+	return nil
+}
---
+++ internal/pkg/config/file_test.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveAndLoadRuleSet(t *testing.T) {
+	rs := RuleSet{
+		Rules: []Rule{
+			{
+				Name:    "default",
+				Pattern: "**",
+				Config: Config{
+					SyntaxLanguage: "undefined",
+				},
+			},
+			{
+				Name:    "json",
+				Pattern: "**/*.json",
+				Config: Config{
+					SyntaxLanguage: "json",
+				},
+			},
+		},
+	}
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	path := path.Join(tmpDir, "aretext", "config.json")
+	err = SaveRuleSet(path, rs)
+	require.NoError(t, err)
+
+	loadedRs, err := LoadRuleSet(path)
+	require.NoError(t, err)
+	assert.Equal(t, rs, loadedRs)
+}

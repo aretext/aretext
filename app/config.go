a3d24c0a1b958526e66be7364cd8db81bf2560f6
Will Daly <will.e.daly@gmail.com>
2021-03-23 08:34:17 -0700

    Use go:embed to create default config
    Previously, aretext would serialize an in-memory config ruleset
    to create the default config.  This could lead to subtle bugs
    where the in-memory representation differed from the serialized
    representation (for example, the in-memory representation might
    have a slice of strings, but the deserialized representation would
    convert this to a slice of interface{}).
    
    We also eventually want to switch the config format to TOML
    and add comments, which would be difficult to do with the in-memory
    representation.
    
    To fix these issues, embed the default config directly in the binary.
    This allows us to control exactly what the config file contains
    and eliminates issues with roundtrip serialization.

--- app/config.go
+++ app/config.go
@@ -1,21 +1,25 @@
 package app
 
 import (
+	_ "embed"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/aretext/aretext/config"
-	"github.com/aretext/aretext/syntax"
 	"github.com/pkg/errors"
 )
+
+//go:embed default-config.json
+var DefaultConfigJson []byte
 
 // LoadOrCreateConfig loads the config file if it exists and creates a default config file otherwise.
 func LoadOrCreateConfig(forceDefaultConfig bool) (config.RuleSet, error) {
 	if forceDefaultConfig {
 		log.Printf("Using default config\n")
-		return defaultConfigRuleSet(), nil
+		return unmarshalRuleSet(DefaultConfigJson)
 	}
 
 	path, err := defaultPath()
@@ -24,20 +28,18 @@ func LoadOrCreateConfig(forceDefaultConfig bool) (config.RuleSet, error) {
 	}
 
 	log.Printf("Loading config from '%s'\n", path)
-
-	rs, err := config.LoadRuleSet(path)
+	data, err := os.ReadFile(path)
 	if os.IsNotExist(err) {
 		log.Printf("Writing default config to '%s'\n", path)
-		rs = defaultConfigRuleSet()
-		err = config.SaveRuleSet(path, rs)
-		if err != nil {
-			log.Printf("Could not save config to '%s': %v\n", path, errors.Wrapf(err, "config.SaveRuleSet"))
+		if err := os.WriteFile(path, DefaultConfigJson, 0644); err != nil {
+			return config.RuleSet{}, errors.Wrapf(err, fmt.Sprintf("Error writing default config to '%s'", path))
 		}
+		return unmarshalRuleSet(DefaultConfigJson)
 	} else if err != nil {
 		return config.RuleSet{}, errors.Wrapf(err, fmt.Sprintf("Error loading config from '%s'", path))
 	}
 
-	return rs, nil
+	return unmarshalRuleSet(data)
 }
 
 // defaultPath returns the path to the user's configuration file.
@@ -50,40 +52,10 @@ func defaultPath() (string, error) {
 	return path, nil
 }
 
-func defaultConfigRuleSet() config.RuleSet {
-	return config.RuleSet{
-		Rules: []config.Rule{
-			{
-				Name:    "default",
-				Pattern: "**",
-				Config: map[string]interface{}{
-					"syntaxLanguage":  syntax.LanguageUndefined.String(),
-					"tabSize":         4,
-					"tabExpand":       false,
-					"autoIndent":      false,
-					"hideDirectories": []interface{}{".git"},
-				},
-			},
-			{
-				Name:    "json",
-				Pattern: "**/*.json",
-				Config: map[string]interface{}{
-					"syntaxLanguage": syntax.LanguageJson.String(),
-					"tabSize":        2,
-					"tabExpand":      true,
-					"autoIndent":     true,
-				},
-			},
-			{
-				Name:    "go",
-				Pattern: "**/*.go",
-				Config: map[string]interface{}{
-					"syntaxLanguage": syntax.LanguageGo.String(),
-					"tabSize":        4,
-					"tabExpand":      false,
-					"autoIndent":     true,
-				},
-			},
-		},
+func unmarshalRuleSet(data []byte) (config.RuleSet, error) {
+	var rules []config.Rule
+	if err := json.Unmarshal(data, &rules); err != nil {
+		return config.RuleSet{}, errors.Wrapf(err, "json.Unmarshal")
 	}
+	return config.RuleSet{Rules: rules}, nil
 }
---
+++ app/config_test.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefaultConfigJsonValid(t *testing.T) {
+	rs, err := unmarshalRuleSet(DefaultConfigJson)
+	require.NoError(t, err)
+	assert.Greater(t, len(rs.Rules), 1)
+
+	c := rs.ConfigForPath("test.go")
+	assert.Equal(t, "go", c.SyntaxLanguage)
+	assert.Equal(t, 4, c.TabSize)
+	assert.True(t, c.AutoIndent)
+}

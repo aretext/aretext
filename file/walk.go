3d7a728c313c87086d9e51069154055873c522b9
Will Daly <will.e.daly@gmail.com>
2021-04-17 13:56:56 -0700

    Use filepath.WalkDir in file search
    Go 1.16 adds a more efficient routine to walk the filesystem.
    See https://github.com/golang/go/issues/42027
    
    This speeds up traversal of a directory with 70K files from
    ~1.3 seconds to ~0.9 seconds.

--- file/walk.go
+++ file/walk.go
@@ -1,31 +1,27 @@
 package file
 
 import (
+	"io/fs"
 	"log"
-	"os"
 	"path/filepath"
-
-	"github.com/pkg/errors"
 )
 
 // Walk walks the file tree at root, evaluating the function for every file path.
 // Symlinks are skipped.
 // The file paths are absolute.
 func Walk(root string, dirNamesToHide map[string]struct{}, walkFn func(path string)) {
-	stack := []string{root}
-	var dirPath string
-	for len(stack) > 0 {
-		dirPath, stack = stack[len(stack)-1], stack[:len(stack)-1]
-		if shouldSkipDir(dirPath, dirNamesToHide) {
-			continue
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			if shouldSkipDir(path, dirNamesToHide) {
+				return fs.SkipDir
+			}
+			return nil
 		}
-
-		subdirPaths, err := processDir(dirPath, walkFn)
-		if err != nil {
-			log.Printf("Error processing directory at '%s': %v\n", dirPath, err)
-			continue
-		}
-		stack = append(stack, subdirPaths...)
+		walkFn(path)
+		return nil
+	})
+	if err != nil {
+		log.Printf("Error walking directory '%s': %v\n", root, err)
 	}
 }
 
@@ -33,31 +29,4 @@ func shouldSkipDir(path string, dirNamesToHide map[string]struct{}) bool {
 	name := filepath.Base(path)
 	_, ok := dirNamesToHide[name]
 	return ok
-}
-
-func processDir(dirPath string, walkFn func(path string)) ([]string, error) {
-	f, err := os.Open(dirPath)
-	if err != nil {
-		return nil, errors.Wrapf(err, "os.Open")
-	}
-	defer f.Close()
-
-	// This is faster than calling LStat on every file, which is what filepath.Walk does.
-	// See discussion here: https://github.com/golang/go/issues/16399
-	fileInfos, err := f.Readdir(-1)
-	if err != nil {
-		return nil, errors.Wrapf(err, "File.Readdir")
-	}
-
-	subdirPaths := make([]string, 0)
-	for _, fi := range fileInfos {
-		absPath := filepath.Join(dirPath, fi.Name())
-		if fi.Mode().IsRegular() {
-			walkFn(absPath)
-		} else if fi.Mode().IsDir() {
-			subdirPaths = append(subdirPaths, absPath)
-		}
-	}
-
-	return subdirPaths, nil
 }

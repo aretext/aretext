037d26cd0d3b756dc95a9586db593f9b124864f5
Will Daly <will.e.daly@gmail.com>
2021-05-06 06:43:07 -0700

    Revert "Menu item clears selection after execution, not before"
    This reverts commit be9419cf20bd924c413d705cd1659429e25098a0.
    
    Commit introduced a bug with the "find and open" menu command.
    The command sets the input mode to "menu", but the change in
    this commit sets it back to "normal", so the user can't
    select any items in the menu.
    
    Reverting this until I can figure out a better fix.

--- state/menu.go
+++ state/menu.go
@@ -94,18 +94,18 @@ func ExecuteSelectedMenuItem(state *EditorState) {
 	log.Printf("Executing menu item '%s' at result index %d\n", selectedItem.Name, idx)
 	HideMenu(state)
 
+	// The action could invalidate the current selection
+	// (for example, by inserting text or opening a new document)
+	// so clear the current selection (if any) and return to normal mode.
+	state.documentBuffer.selector.Clear()
+	SetInputMode(state, InputModeNormal)
+
 	actionFunc, ok := selectedItem.Action.(func(*EditorState))
 	if !ok {
 		log.Printf("Invalid action for menu item '%s'\n", selectedItem.Name)
 		return
 	}
 	actionFunc(state)
-
-	// The action could have invalidated the current selection
-	// (for example, by inserting text or opening a new document)
-	// so clear the current selection (if any) and return to normal mode.
-	state.documentBuffer.selector.Clear()
-	SetInputMode(state, InputModeNormal)
 }
 
 // MoveMenuSelection moves the menu selection up or down with wraparound.

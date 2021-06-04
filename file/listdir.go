578806b7b052d418ac93544539729ff6e27ae1a8
Will Daly <will.e.daly@gmail.com>
2021-06-04 07:07:29 -0700

    Add file.ListDir helper
    ListDir walks a file tree and returns all paths.
    It is significantly faster than WalkDir because it
    traverses subdirectories concurrently.

---
+++ file/listdir.go
@@ -0,0 +1,77 @@
+package file
+
+import (
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sync"
+
+	"github.com/pkg/errors"
+)
+
+// ListDir lists every file in a root directory and its subdirectories.
+// The returned paths are relative to the root directory.
+// The order of the returned paths is non-deterministic.
+// Symbolic links are not followed.
+// If an error occurs while accessing a directory, ListDir will skip that
+// directory and log the error.
+func ListDir(root string, dirNamesToHide map[string]struct{}) []string {
+	// Use a semaphore to limit the number of open files.
+	semaphoreChan := make(chan struct{}, runtime.NumCPU())
+	return listDirRec(root, dirNamesToHide, semaphoreChan)
+}
+
+func listDirRec(root string, dirNamesToHide map[string]struct{}, semaphoreChan chan struct{}) []string {
+	semaphoreChan <- struct{}{} // Block until open file count decreases.
+	dirEntries, err := listDir(root)
+	<-semaphoreChan // Decrease open file count.
+
+	if err != nil {
+		log.Printf("Error listing subdirectories in '%s': %s\n", root, err)
+		return nil
+	}
+
+	var mu sync.Mutex
+	var results []string
+	var wg sync.WaitGroup
+	for _, d := range dirEntries {
+		path := filepath.Join(root, d.Name())
+
+		if !d.IsDir() {
+			results = append(results, path)
+			continue
+		}
+
+		if shouldSkipDir(path, dirNamesToHide) {
+			continue
+		}
+
+		// Traverse subdirectories concurrently.
+		wg.Add(1)
+		go func(path string) {
+			defer wg.Done()
+			subpaths := listDirRec(path, dirNamesToHide, semaphoreChan)
+			mu.Lock()
+			results = append(results, subpaths...)
+			mu.Unlock()
+		}(path)
+	}
+	wg.Wait()
+
+	return results
+}
+
+func listDir(path string) ([]fs.DirEntry, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "os.Open")
+	}
+	dirs, err := f.ReadDir(-1)
+	f.Close()
+	if err != nil {
+		return nil, errors.Wrapf(err, "f.ReadDir")
+	}
+	return dirs, nil
+}
---
+++ file/listdir_test.go
@@ -0,0 +1,68 @@
+package file
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListDir(t *testing.T) {
+	// Create a tmpdir (and delete at end of test).
+	tmpDir, err := filepath.Abs("./tmp")
+	require.NoError(t, err)
+	err = os.MkdirAll(tmpDir, 0755)
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	// Set the cwd to the tmpdir (and reset at end of test).
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+	err = os.Chdir(tmpDir)
+	require.NoError(t, err)
+
+	// Create subdirectories and files in the tmpdir.
+	paths := []string{
+		"a/1.txt",
+		"a/2.txt",
+		"b/2.txt",
+		"a/b/4.txt",
+		"a/.hidden/1.txt",
+		"a/.hidden/2.txt",
+	}
+
+	for _, p := range paths {
+		fullPath := filepath.Join(tmpDir, p)
+		err = os.MkdirAll(filepath.Dir(fullPath), 0755)
+		require.NoError(t, err)
+		err = ioutil.WriteFile(fullPath, []byte("test"), 0644)
+		require.NoError(t, err)
+	}
+
+	dirNamesToHide := map[string]struct{}{
+		".hidden": struct{}{},
+	}
+
+	// List all paths in tmpdir.
+	foundPaths := ListDir(tmpDir, dirNamesToHide)
+	relPaths := make([]string, 0, len(foundPaths))
+	for _, p := range foundPaths {
+		relPaths = append(relPaths, RelativePathCwd(p))
+	}
+
+	// Check that we found all the paths we created (except hidden ones), ignoring order.
+	expectedPaths := []string{
+		"a/1.txt",
+		"a/2.txt",
+		"b/2.txt",
+		"a/b/4.txt",
+	}
+	sort.Strings(expectedPaths)
+	sort.Strings(relPaths)
+	assert.Equal(t, expectedPaths, relPaths)
+}

b277e4954575d1d89e1a866a401786082fc0041e
Will Daly <will.e.daly@gmail.com>
2021-07-01 17:12:58 -0700

    Toggle commands show status message
    Show status messages for all toggle menu commands.
    
    Resolves: https://github.com/aretext/aretext/issues/74

--- state/config.go
+++ state/config.go
@@ -2,15 +2,31 @@ package state
 
 // ToggleShowTabs shows or hides tab characters in the document.
 func ToggleShowTabs(s *EditorState) {
-	s.documentBuffer.showTabs = !s.documentBuffer.showTabs
+	toggleFlagAndSetStatus(s, &s.documentBuffer.showTabs, "Showing tabs", "Hiding tabs")
 }
 
 // ToggleShowLineNumbers shows or hides line numbers in the left margin.
 func ToggleShowLineNumbers(s *EditorState) {
-	s.documentBuffer.showLineNum = !s.documentBuffer.showLineNum
+	toggleFlagAndSetStatus(s, &s.documentBuffer.showLineNum, "Showing line numbers", "Hiding line numbers")
 }
 
 // ToggleAutoIndent enables or disables auto-indent.
 func ToggleAutoIndent(s *EditorState) {
-	s.documentBuffer.autoIndent = !s.documentBuffer.autoIndent
+	toggleFlagAndSetStatus(s, &s.documentBuffer.autoIndent, "Enabled auto-indent", "Disabled auto-indent")
+}
+
+func toggleFlagAndSetStatus(s *EditorState, flagValue *bool, enabledMsg string, disabledMsg string) {
+	*flagValue = !(*flagValue)
+
+	var msg string
+	if *flagValue {
+		msg = enabledMsg
+	} else {
+		msg = disabledMsg
+	}
+
+	SetStatusMsg(s, StatusMsg{
+		Style: StatusMsgStyleSuccess,
+		Text:  msg,
+	})
 }

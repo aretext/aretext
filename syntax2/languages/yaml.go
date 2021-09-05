3b2f8586663441a05a1df87b5559e582172a6615
Will Daly <will.e.daly@gmail.com>
2021-09-05 14:22:36 -0700

    Implement new YAML parser

---
+++ syntax2/languages/yaml.go
@@ -0,0 +1,86 @@
+package languages
+
+import (
+	"github.com/aretext/aretext/syntax2/parser"
+)
+
+// YamlParseFunc returns a parse func for YAML.
+func YamlParseFunc() parser.Func {
+	return yamlSingleQuoteStringOrKeyParseFunc().
+		Or(yamlCommentParseFunc()).
+		Or(yamlKeyParseFunc()).
+		Or(JsonParseFunc()) // YAML is a superset of JSON.
+}
+
+const yamlTokenRoleKey = parser.TokenRoleCustom1
+
+func yamlSingleQuoteStringParseFunc() parser.Func {
+	// Parse string wrapped in single-quotes, but treat '' as an escaped quote.
+	return func(iter parser.TrackingRuneIter, state parser.State) parser.Result {
+		// Consume first single-quote.
+		var n uint64
+		r, err := iter.NextRune()
+		if err != nil || r != '\'' {
+			return parser.FailedResult
+		}
+		n++
+
+		// Consume the rest of the string.
+		for {
+			r, err = iter.NextRune()
+			if err != nil || r == '\n' {
+				// Couldn't find a closing quote.
+				return parser.FailedResult
+			}
+			n++
+
+			if r == '\'' {
+				// Found a single quote.
+				r, err = iter.NextRune()
+				if err != nil || r != '\'' {
+					// Found the end of the string.
+					return parser.Result{
+						NumConsumed: n,
+						ComputedTokens: []parser.ComputedToken{
+							{
+								Length: n,
+								Role:   parser.TokenRoleString,
+							},
+						},
+						NextState: state,
+					}
+				}
+
+				// The next rune is also a quote, so this is an escaped quote.
+				// Consume it and keep going.
+				n++
+			}
+		}
+	}
+}
+
+func yamlSingleQuoteStringOrKeyParseFunc() parser.Func {
+	return yamlSingleQuoteStringParseFunc().
+		ThenMaybe(jsonConsumeToKeyEndParseFunc()).
+		Map(func(r parser.Result) parser.Result {
+			if len(r.ComputedTokens) == 1 && r.NumConsumed > r.ComputedTokens[0].Length {
+				// Must have parsed additional characters after the end of the string,
+				// so this is a key.
+				return recognizeToken(yamlTokenRoleKey)(r)
+			} else {
+				return r
+			}
+		})
+}
+
+func yamlCommentParseFunc() parser.Func {
+	return consumeString("#").
+		Then(consumeToEndOfLine).
+		Map(recognizeToken(parser.TokenRoleComment))
+}
+
+func yamlKeyParseFunc() parser.Func {
+	return consumeRunesLike(jsonIdentifierRune).
+		Then(jsonConsumeToKeyEndParseFunc()).
+		Map(recognizeToken(yamlTokenRoleKey))
+}
---
+++ syntax2/languages/yaml_test.go
@@ -0,0 +1,86 @@
+package languages
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aretext/aretext/syntax2/parser"
+)
+
+func TestYamlTokenizer(t *testing.T) {
+	const tokenRoleKey = parser.TokenRoleCustom1
+	testCases := []struct {
+		name     string
+		text     string
+		expected []TokenWithText
+	}{
+		{
+			name: "single line comment",
+			text: `# abc`,
+			expected: []TokenWithText{
+				{Text: `# abc`, Role: parser.TokenRoleComment},
+			},
+		},
+		{
+			name: "key without quotes",
+			text: "abc: xyz",
+			expected: []TokenWithText{
+				{Text: `abc:`, Role: tokenRoleKey},
+			},
+		},
+		{
+			name: "key with single-quoted string",
+			text: "'abc': xyz",
+			expected: []TokenWithText{
+				{Text: `'abc':`, Role: tokenRoleKey},
+			},
+		},
+		{
+			name: "empty single-quoted string",
+			text: `''`,
+			expected: []TokenWithText{
+				{Text: `''`, Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "single-quoted string",
+			text: `'abc'`,
+			expected: []TokenWithText{
+				{Text: `'abc'`, Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "single-quoted string with escaped quote",
+			text: `'ab''c'`,
+			expected: []TokenWithText{
+				{Text: `'ab''c'`, Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "doc with keys, number, and comment",
+			text: `- foo: # this is a test
+							bar: 123`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: tokenRoleKey},
+				{Text: "# this is a test\n", Role: parser.TokenRoleComment},
+				{Text: `bar:`, Role: tokenRoleKey},
+				{Text: `123`, Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "unquoted string with number suffix",
+			text: "foo: v0.1.2",
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: tokenRoleKey},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tokens := ParseTokensWithText(YamlParseFunc(), tc.text)
+			assert.Equal(t, tc.expected, tokens)
+		})
+	}
+}

0c16c1188dcaee4d73d8c53421f6b731b1068659
Will Daly <will.e.daly@gmail.com>
2021-11-03 07:23:13 -0700

    Add syntax highlighting for C
    Resolves https://github.com/aretext/aretext/issues/81

---
+++ syntax/languages/c.go
@@ -0,0 +1,158 @@
+package languages
+
+import (
+	"unicode"
+
+	"github.com/aretext/aretext/syntax/parser"
+)
+
+const cTokenRolePreprocessorDirective = parser.TokenRoleCustom1
+
+// CParseFunc returns a parse func for C99 with GNU extensions.
+// See "The GNU C Reference Manual"
+// http://www.gnu.org/software/gnu-c-manual/gnu-c-manual.html#Lexical-Elements
+// and "The C Preprocessor"
+// http://gcc.gnu.org/onlinedocs/cpp/
+func CParseFunc() parser.Func {
+	return cCommentParseFunc().
+		Or(cPreprocessorDirective()).
+		Or(cIdentifierOrKeywordParseFunc()).
+		Or(cOperatorParseFunc()).
+		Or(cStringParseFunc()).
+		Or(cNumberParseFunc())
+}
+
+func cCommentParseFunc() parser.Func {
+	consumeLineComment := consumeString("//").
+		ThenMaybe(consumeToNextLineFeed)
+
+	consumeBlockComment := consumeString("/*").
+		Then(consumeToString("*/"))
+
+	return consumeLineComment.
+		Or(consumeBlockComment).
+		Map(recognizeToken(parser.TokenRoleComment))
+}
+
+func cPreprocessorDirective() parser.Func {
+	return consumeString("#").
+		Then(consumeString("include").
+			Or(consumeString("pragma")).
+			Or(consumeString("ifndef")).
+			Or(consumeString("define")).
+			Or(consumeString("error")).
+			Or(consumeString("undef")).
+			Or(consumeString("endif")).
+			Or(consumeString("ifdef")).
+			Or(consumeString("elif")).
+			Or(consumeString("else")).
+			Or(consumeString("if"))).
+		ThenNot(consumeSingleRuneLike(func(r rune) bool {
+			return !unicode.IsSpace(r) // must be followed by space, newline, or EOF
+		})).
+		ThenMaybe(consumeToNextLineFeed).
+		Map(recognizeToken(cTokenRolePreprocessorDirective))
+}
+
+func cIdentifierOrKeywordParseFunc() parser.Func {
+	isIdStart := func(r rune) bool {
+		return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || r == '_' || r == '$'
+	}
+
+	isIdContinue := func(r rune) bool {
+		return isIdStart(r) || (r >= '0' && r <= '9')
+	}
+
+	keywords := []string{
+		"auto", "break", "case", "char", "const", "continue",
+		"default", "do", "double", "else", "enum", "extern",
+		"float", "for", "goto", "if", "int", "long", "register",
+		"return", "short", "signed", "sizeof", "static",
+		"struct", "switch", "typedef", "union", "unsigned",
+		"void", "volatile", "while",
+		"inline", "_Bool", "_Complex", "_Imaginary",
+		"__FUNCTION__", "__PRETTY_FUNCTION__", "__alignof", "__alignof__", "__asm",
+		"__asm__", "__attribute", "__attribute__", "__builtin_offsetof",
+		"__builtin_va_arg", "__complex", "__complex__", "__const",
+		"__extension__", "__func__", "__imag", "__imag__",
+		"__inline", "__inline__", "__label__", "__null", "__real", "__real__",
+		"__restrict", "__restrict__", "__signed", "__signed__", "__thread", "__typeof",
+		"__volatile", "__volatile__",
+		"restrict",
+	}
+
+	recognizeIdentifier := recognizeToken(parser.TokenRoleIdentifier)
+	return consumeSingleRuneLike(isIdStart).
+		ThenMaybe(consumeRunesLike(isIdContinue)).
+		MapWithInput(recognizeKeywordOrConsume(keywords)).
+		Map(func(r parser.Result) parser.Result {
+			if len(r.ComputedTokens) == 0 {
+				return recognizeIdentifier(r)
+			}
+			return r
+		})
+}
+
+func cOperatorParseFunc() parser.Func {
+	return (consumeString("=").ThenMaybe(consumeString("="))).
+		Or(consumeString("+").ThenMaybe(consumeString("=").Or(consumeString("+")))).
+		Or(consumeString("-").ThenMaybe(consumeString("=").Or(consumeString("-")))).
+		Or(consumeString("*").ThenMaybe(consumeString("="))).
+		Or(consumeString("/").ThenMaybe(consumeString("="))).
+		Or(consumeString("%").ThenMaybe(consumeString("="))).
+		Or(consumeString("<").ThenMaybe(consumeString("="))).
+		Or(consumeString(">").ThenMaybe(consumeString("="))).
+		Or(consumeString("<<").ThenMaybe(consumeString("="))).
+		Or(consumeString(">>").ThenMaybe(consumeString("="))).
+		Or(consumeString("^").ThenMaybe(consumeString("="))).
+		Or(consumeString("|").ThenMaybe(consumeString("=").Or(consumeString("|")))).
+		Or(consumeString("~")).
+		Or(consumeString("!").ThenMaybe(consumeString("="))).
+		Or(consumeString("&").ThenMaybe(consumeString("=").Or(consumeString("&")))).
+		Or(consumeString("sizeof")).
+		Map(recognizeToken(parser.TokenRoleOperator))
+}
+
+func cStringParseFunc() parser.Func {
+	return consumeCStyleString('\'').
+		Or(consumeCStyleString('"')).
+		Map(recognizeToken(parser.TokenRoleString))
+}
+
+func cNumberParseFunc() parser.Func {
+	isDigit := func(r rune) bool { return r >= '0' && r <= '9' }
+	isHex := func(r rune) bool {
+		return isDigit(r) || (r >= 'a' && r <= 'f') || (r >= 'A' && r <= 'F')
+	}
+	consumeIntTypeSuffix := consumeString("ULL").
+		Or(consumeString("LL")).
+		Or(consumeString("u")).
+		Or(consumeString("U")).
+		Or(consumeString("l")).
+		Or(consumeString("L"))
+
+	consumeHex := consumeString("0x").
+		Then(consumeRunesLike(isHex)).
+		ThenMaybe(consumeIntTypeSuffix)
+
+	consumeDecimal := consumeRunesLike(isDigit) // Implicitly handles octal (0 at start)
+	consumeInteger := consumeDecimal.ThenMaybe(consumeIntTypeSuffix)
+
+	consumeExponent := (consumeString("e").Or(consumeString("E"))).
+		ThenMaybe(consumeString("-")).
+		Then(consumeDecimal)
+	consumeRealTypeSuffix := consumeString("l").
+		Or(consumeString("L")).
+		Or(consumeString("f")).
+		Or(consumeString("F"))
+	consumeRealWithDecimal := (consumeString(".").Then(consumeDecimal)).
+		Or(consumeDecimal.Then(consumeString(".")).ThenMaybe(consumeDecimal)).
+		ThenMaybe(consumeExponent)
+	consumeRealJustExponent := consumeDecimal.Then(consumeExponent)
+	consumeReal := consumeRealWithDecimal.
+		Or(consumeRealJustExponent).
+		ThenMaybe(consumeRealTypeSuffix)
+
+	return consumeHex.Or(consumeReal).Or(consumeInteger).
+		Map(recognizeToken(parser.TokenRoleNumber))
+}
---
+++ syntax/languages/c_test.go
@@ -0,0 +1,165 @@
+package languages
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aretext/aretext/syntax/parser"
+)
+
+func TestCParseFunc(t *testing.T) {
+	testCases := []struct {
+		name     string
+		text     string
+		expected []TokenWithText
+	}{
+		{
+			name: "hello world",
+			text: `
+#include <stdio.h>
+
+main()
+{
+    printf("hello, world\n");
+}`,
+			expected: []TokenWithText{
+				{Text: "#include <stdio.h>\n", Role: cTokenRolePreprocessorDirective},
+				{Text: "main", Role: parser.TokenRoleIdentifier},
+				{Text: "printf", Role: parser.TokenRoleIdentifier},
+				{Text: `"hello, world\n"`, Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "conditionals and math",
+			text: `if (x % 2 == 0) { return x + 10 } else { return x / 2 }`,
+			expected: []TokenWithText{
+				{Text: "if", Role: parser.TokenRoleKeyword},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "%", Role: parser.TokenRoleOperator},
+				{Text: "2", Role: parser.TokenRoleNumber},
+				{Text: "==", Role: parser.TokenRoleOperator},
+				{Text: "0", Role: parser.TokenRoleNumber},
+				{Text: "return", Role: parser.TokenRoleKeyword},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "+", Role: parser.TokenRoleOperator},
+				{Text: "10", Role: parser.TokenRoleNumber},
+				{Text: "else", Role: parser.TokenRoleKeyword},
+				{Text: "return", Role: parser.TokenRoleKeyword},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "/", Role: parser.TokenRoleOperator},
+				{Text: "2", Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "header guard with comments",
+			text: `
+#ifndef _FILE_NAME_H_
+#define _FILE_NAME_H_
+
+/* code */
+
+#endif // #ifndef _FILE_NAME_H_`,
+			expected: []TokenWithText{
+				{Text: "#ifndef _FILE_NAME_H_\n", Role: cTokenRolePreprocessorDirective},
+				{Text: "#define _FILE_NAME_H_\n", Role: cTokenRolePreprocessorDirective},
+				{Text: "/* code */", Role: parser.TokenRoleComment},
+				{Text: "#endif // #ifndef _FILE_NAME_H_", Role: cTokenRolePreprocessorDirective},
+			},
+		},
+		{
+			name: "bit twiddling hacks",
+			text: `r = x ^ ((x ^ y) & -(x < y)); // max(x, y)`,
+			expected: []TokenWithText{
+				{Text: "r", Role: parser.TokenRoleIdentifier},
+				{Text: "=", Role: parser.TokenRoleOperator},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "^", Role: parser.TokenRoleOperator},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "^", Role: parser.TokenRoleOperator},
+				{Text: "y", Role: parser.TokenRoleIdentifier},
+				{Text: "&", Role: parser.TokenRoleOperator},
+				{Text: "-", Role: parser.TokenRoleOperator},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "<", Role: parser.TokenRoleOperator},
+				{Text: "y", Role: parser.TokenRoleIdentifier},
+				{Text: "// max(x, y)", Role: parser.TokenRoleComment},
+			},
+		},
+		{
+			name: "integer and char declaration",
+			text: `
+int foo;
+unsigned int bar = 42;
+char quux = 'a';`,
+			expected: []TokenWithText{
+				{Text: "int", Role: parser.TokenRoleKeyword},
+				{Text: "foo", Role: parser.TokenRoleIdentifier},
+				{Text: "unsigned", Role: parser.TokenRoleKeyword},
+				{Text: "int", Role: parser.TokenRoleKeyword},
+				{Text: "bar", Role: parser.TokenRoleIdentifier},
+				{Text: "=", Role: parser.TokenRoleOperator},
+				{Text: "42", Role: parser.TokenRoleNumber},
+				{Text: "char", Role: parser.TokenRoleKeyword},
+				{Text: "quux", Role: parser.TokenRoleIdentifier},
+				{Text: "=", Role: parser.TokenRoleOperator},
+				{Text: "'a'", Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "real number declaration",
+			text: `
+float foo;
+double bar = 114.3943;`,
+			expected: []TokenWithText{
+				{Text: "float", Role: parser.TokenRoleKeyword},
+				{Text: "foo", Role: parser.TokenRoleIdentifier},
+				{Text: "double", Role: parser.TokenRoleKeyword},
+				{Text: "bar", Role: parser.TokenRoleIdentifier},
+				{Text: "=", Role: parser.TokenRoleOperator},
+				{Text: "114.3943", Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "hex number with suffix",
+			text: "0xABCULL",
+			expected: []TokenWithText{
+				{Text: "0xABCULL", Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "float with exponent",
+			text: `
+double x, y;
+
+x = 5e2;
+y = 5e-2;
+`,
+			expected: []TokenWithText{
+				{Text: "double", Role: parser.TokenRoleKeyword},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "y", Role: parser.TokenRoleIdentifier},
+				{Text: "x", Role: parser.TokenRoleIdentifier},
+				{Text: "=", Role: parser.TokenRoleOperator},
+				{Text: "5e2", Role: parser.TokenRoleNumber},
+				{Text: "y", Role: parser.TokenRoleIdentifier},
+				{Text: "=", Role: parser.TokenRoleOperator},
+				{Text: "5e-2", Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "unrecognized preprocessor directive",
+			text: "#endifaaa",
+			expected: []TokenWithText{
+				{Text: "endifaaa", Role: parser.TokenRoleIdentifier},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tokens := ParseTokensWithText(CParseFunc(), tc.text)
+			assert.Equal(t, tc.expected, tokens)
+		})
+	}
+}

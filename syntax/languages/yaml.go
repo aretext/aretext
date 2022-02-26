9b947e096db1f86fbe2b30a64f30b9f137654f19
Will Daly <will.e.daly@gmail.com>
2022-02-26 15:02:40 -0800

    Improve accuracy of YAML syntax highlighting
    The initial YAML syntax highlighting implementation
    often misinterpreted multi-line scalars. It also
    failed to recognize block style / chomping operators
    or aliases/targets/overrides.
    
    This commit reimplements the YAML parser, using the new
    parsing framework to handle these cases much more accurately.
    It maintains a stack of flow-style delimiters (open/close braces/brackets)
    to accurately track whether to use flow- or block-style parsing.
    
    The new parser doesn't implement every part of the YAML spec,
    but it's a good-enough approximation. Other editors I tested
    seem to be using similar hacks.
    
    Resolves: https://github.com/aretext/aretext/issues/75

--- syntax/languages/yaml.go
+++ syntax/languages/yaml.go
@@ -1,76 +1,350 @@
 package languages
 
 import (
+	"unicode"
+
 	"github.com/aretext/aretext/syntax/parser"
+)
+
+const (
+	yamlTokenRoleKey           = parser.TokenRoleCustom1
+	yamlTokenRoleAliasOrAnchor = parser.TokenRoleCustom2
 )
 
 // YamlParseFunc returns a parse func for YAML.
 func YamlParseFunc() parser.Func {
-	return yamlSingleQuoteStringOrKeyParseFunc().
-		Or(yamlCommentParseFunc()).
+	parseBlockStyle := yamlTransitionToFlowParseFunc().
 		Or(yamlKeyParseFunc()).
-		Or(JsonParseFunc()) // YAML is a superset of JSON.
+		Or(yamlOverrideParseFunc()).
+		Or(yamlListParseFunc()).
+		Or(yamlAnchorAliasParseFunc()).
+		Or(yamlCommentParseFunc()).
+		Or(yamlBlockScalarParseFunc())
+
+	parseFlowStyle := yamlFlowStyleBracketsAndBracesParseFunc().
+		Or(yamlKeyParseFunc()).
+		Or(yamlCommentParseFunc()).
+		Or(yamlFlowScalarParseFunc())
+
+	return initialState(
+		yamlParseState{},
+		func(iter parser.TrackingRuneIter, state parser.State) parser.Result {
+			yamlState := state.(yamlParseState)
+			if len(yamlState.flowStyleStack) > 0 {
+				return parseFlowStyle(iter, state)
+			} else {
+				return parseBlockStyle(iter, state)
+			}
+		})
 }
 
-const yamlTokenRoleKey = parser.TokenRoleCustom1
+func yamlSkipIndentation(f parser.Func) parser.Func {
+	consumeIndentation := consumeRunesLike(func(r rune) bool {
+		return r == ' ' || r == '\t'
+	})
+	return consumeIndentation.MaybeBefore(f)
+}
 
-func yamlSingleQuoteStringParseFunc() parser.Func {
-	// Parse string wrapped in single-quotes, but treat '' as an escaped quote.
-	return func(iter parser.TrackingRuneIter, state parser.State) parser.Result {
-		// Consume first single-quote.
-		var n uint64
-		r, err := iter.NextRune()
-		if err != nil || r != '\'' {
-			return parser.FailedResult
+func yamlTransitionToFlowParseFunc() parser.Func {
+	parseFlowMapStart := consumeString("{").
+		Map(setState(yamlParseState{flowStyleStack: []yamlFlowStyle{yamlFlowStyleMap}}))
+
+	parseFlowArrayStart := consumeString("[").
+		Map(setState(yamlParseState{flowStyleStack: []yamlFlowStyle{yamlFlowStyleArray}}))
+
+	return yamlSkipIndentation(parseFlowMapStart.Or(parseFlowArrayStart))
+}
+
+func yamlFlowStyleBracketsAndBracesParseFunc() parser.Func {
+	pushFlowStack := func(flowState yamlFlowStyle) parser.MapFn {
+		return func(result parser.Result) parser.Result {
+			yamlState := result.NextState.(yamlParseState)
+			yamlState.flowStyleStack = append(yamlState.flowStyleStack, flowState)
+			return parser.Result{
+				NumConsumed:    result.NumConsumed,
+				ComputedTokens: result.ComputedTokens,
+				NextState:      parser.State(yamlState),
+			}
 		}
-		n++
+	}
 
-		// Consume the rest of the string.
-		for {
-			r, err = iter.NextRune()
-			if err != nil || r == '\n' {
-				// Couldn't find a closing quote.
+	popFlowStack := func(flowState yamlFlowStyle) parser.MapFn {
+		return func(result parser.Result) parser.Result {
+			yamlState := result.NextState.(yamlParseState)
+			n := len(yamlState.flowStyleStack)
+			if n == 0 || yamlState.flowStyleStack[n-1] != flowState {
+				return result
+			}
+			yamlState.flowStyleStack = yamlState.flowStyleStack[0 : n-1]
+			return parser.Result{
+				NumConsumed:    result.NumConsumed,
+				ComputedTokens: result.ComputedTokens,
+				NextState:      parser.State(yamlState),
+			}
+		}
+	}
+
+	parseOpenBrace := consumeString("{").
+		Map(pushFlowStack(yamlFlowStyleMap))
+
+	parseCloseBrace := consumeString("}").
+		Map(popFlowStack(yamlFlowStyleMap))
+
+	parseOpenBracket := consumeString("[").
+		Map(pushFlowStack(yamlFlowStyleArray))
+
+	parseCloseBracket := consumeString("]").
+		Map(popFlowStack(yamlFlowStyleArray))
+
+	return parseOpenBrace.
+		Or(parseCloseBrace).
+		Or(parseOpenBracket).
+		Or(parseCloseBracket)
+}
+
+func yamlKeyParseFunc() parser.Func {
+	consumeToKeyEnd := jsonConsumeToKeyEndParseFunc()
+
+	parseUnquotedKey := consumeRunesLike(jsonIdentifierRune).
+		Then(consumeToKeyEnd)
+
+	parseQuotedKey := yamlStringParseFunc().Then(consumeToKeyEnd)
+
+	return yamlSkipIndentation(
+		parseUnquotedKey.
+			Or(parseQuotedKey).
+			ThenNot(consumeSingleRuneLike(func(r rune) bool {
+				return !unicode.IsSpace(r) // must be followed by space, newline, or EOF
+			})).
+			Map(recognizeToken(yamlTokenRoleKey)))
+}
+
+func yamlOverrideParseFunc() parser.Func {
+	return yamlSkipIndentation(
+		consumeString("<<:").
+			Map(recognizeToken(parser.TokenRoleOperator)))
+}
+
+func yamlAnchorAliasParseFunc() parser.Func {
+	return yamlSkipIndentation(
+		consumeString("*").
+			Or(consumeString("&")).
+			Then(consumeRunesLike(jsonIdentifierRune)).
+			Map(recognizeToken(yamlTokenRoleAliasOrAnchor)))
+}
+
+func yamlListParseFunc() parser.Func {
+	return yamlSkipIndentation(
+		consumeString("-").
+			ThenNot(consumeSingleRuneLike(func(r rune) bool {
+				return !unicode.IsSpace(r) // must be followed by space, newline, or EOF
+			})).
+			Map(recognizeToken(parser.TokenRoleOperator)))
+}
+
+func yamlStringParseFunc() parser.Func {
+	// Consume string wrapped in single-quotes, but treat '' as an escaped quote.
+	consumeSingleQuotedString := parser.Func(
+		func(iter parser.TrackingRuneIter, state parser.State) parser.Result {
+			// Consume first single-quote.
+			var n uint64
+			r, err := iter.NextRune()
+			if err != nil || r != '\'' {
 				return parser.FailedResult
 			}
 			n++
 
-			if r == '\'' {
-				// Found a single quote.
+			// Consume the rest of the string.
+			for {
 				r, err = iter.NextRune()
-				if err != nil || r != '\'' {
-					// Found the end of the string.
-					return parser.Result{
-						NumConsumed: n,
-						ComputedTokens: []parser.ComputedToken{
-							{
-								Length: n,
-								Role:   parser.TokenRoleString,
-							},
-						},
-						NextState: state,
+				if err != nil || r == '\n' {
+					// Couldn't find a closing quote.
+					return parser.FailedResult
+				}
+				n++
+
+				if r == '\'' {
+					// Found a single quote.
+					r, err = iter.NextRune()
+					if err != nil || r != '\'' {
+						// Found the end of the string.
+						return parser.Result{
+							NumConsumed: n,
+							NextState:   state,
+						}
 					}
+
+					// The next rune is also a quote, so this is an escaped quote.
+					// Consume it and keep going.
+					n++
+				}
+			}
+		})
+
+	return consumeSingleQuotedString.
+		Or(consumeCStyleString('"', false)).
+		Map(recognizeToken(parser.TokenRoleString))
+}
+
+// Match at least two words separated by a space, continuing to the end of the line.
+// Example: "123 abc" matches, but "123" does not.
+func yamlMultiWordUnquotedScalarParseFunc(endRunePredicate func(r rune) bool) parser.Func {
+	return func(iter parser.TrackingRuneIter, state parser.State) parser.Result {
+		var searchState int
+		var n uint64
+		for {
+			r, err := iter.NextRune()
+			if err != nil || endRunePredicate(r) {
+				break
+			}
+			n++
+
+			switch searchState {
+			case 0:
+				// Initially looking for non-space.
+				if !(r == ' ' || r == '\t') {
+					searchState = 1
+				}
+			case 1:
+				// Then look for a space.
+				if r == ' ' || r == '\t' {
+					searchState = 2
 				}
 
-				// The next rune is also a quote, so this is an escaped quote.
-				// Consume it and keep going.
-				n++
+			case 2:
+				// Then look for a non-space again.
+				if !(r == ' ' || r == '\t') {
+					searchState = 3
+				}
 			}
+		}
+
+		if searchState == 3 {
+			return parser.Result{
+				NumConsumed: n,
+				NextState:   state,
+			}
+		} else {
+			return parser.FailedResult
 		}
 	}
 }
 
-func yamlSingleQuoteStringOrKeyParseFunc() parser.Func {
-	return yamlSingleQuoteStringParseFunc().
-		ThenMaybe(jsonConsumeToKeyEndParseFunc()).
-		Map(func(r parser.Result) parser.Result {
-			if len(r.ComputedTokens) == 1 && r.NumConsumed > r.ComputedTokens[0].Length {
-				// Must have parsed additional characters after the end of the string,
-				// so this is a key.
-				return recognizeToken(yamlTokenRoleKey)(r)
-			} else {
-				return r
+func yamlSingleWordUnquotedScalarParseFunc() parser.Func {
+	keywords := []string{"true", "false", "null"}
+
+	yamlScalarRune := func(r rune) bool {
+		return !(unicode.IsSpace(r) || r == '#')
+	}
+
+	return consumeRunesLike(yamlScalarRune).
+		MapWithInput(recognizeKeywordOrConsume(keywords))
+}
+
+func yamlBlockScalarParseFunc() parser.Func {
+	endOfBlockScalarRune := func(r rune) bool {
+		return r == '#' || r == '\n'
+	}
+	parseMultiWordUnquotedScalar := yamlMultiWordUnquotedScalarParseFunc(endOfBlockScalarRune)
+
+	parseBlockStyleIndicator := consumeString("|").Or(consumeString(">")).
+		Map(recognizeToken(parser.TokenRoleOperator))
+
+	parseBlockChompingIndicator := consumeString("-").Or(consumeString("+")).
+		Map(recognizeToken(parser.TokenRoleOperator))
+
+	// Count indentation from the current position.
+	// The YAML spec forbids mixing tabs and spaces in indentation, so we don't differentiate
+	// between them (both increase the indentation level by one).
+	countIndentation := func(iter parser.TrackingRuneIter) uint64 {
+		var n uint64
+		for {
+			r, err := iter.NextRune()
+			if err != nil || !(r == ' ' || r == '\t' || r == '\n') {
+				break
+			}
+			n++
+		}
+		return n
+	}
+
+	skipToEndOfLineOrFile := func(iter *parser.TrackingRuneIter) uint64 {
+		var n uint64
+		for {
+			r, err := iter.NextRune()
+			if err != nil {
+				break
+			}
+			n++
+			if r == '\n' {
+				break
+			}
+		}
+		return n
+	}
+
+	// This is a bit of a hack that most YAML syntax highlighters use.
+	// Suppose we have a block scalar like this:
+	//
+	//    key1:
+	//      first line
+	//      second line
+	//      third line
+	//    key2: 123
+	//
+	// How do we know that the scalar ends before "key2"?
+	// We count the spaces at the start of "first line", then consume every
+	// subsequent line indented by at least that many spaces.
+	consumeBlockLines := parser.Func(
+		func(iter parser.TrackingRuneIter, state parser.State) parser.Result {
+			firstLineIndent := countIndentation(iter)
+			n := skipToEndOfLineOrFile(&iter)
+			for {
+				nextLineIndent := countIndentation(iter)
+				if nextLineIndent < firstLineIndent {
+					break
+				} else {
+					m := skipToEndOfLineOrFile(&iter)
+					if m == 0 {
+						// End of file.
+						break
+					}
+					n += m
+				}
+			}
+
+			return parser.Result{
+				NumConsumed: n,
+				NextState:   state,
 			}
 		})
+
+	parseBlockLines := consumeToNextLineFeed.
+		ThenMaybe(consumeBlockLines).
+		Map(recognizeToken(parser.TokenRoleString))
+
+	parseBlockScalar := parseBlockStyleIndicator.
+		ThenMaybe(parseBlockChompingIndicator).
+		ThenMaybe(parseBlockLines)
+
+	return yamlSkipIndentation(
+		parseBlockScalar.
+			Or(yamlStringParseFunc()).
+			Or(parseMultiWordUnquotedScalar).
+			Or(jsonNumberParseFunc()).
+			Or(yamlSingleWordUnquotedScalarParseFunc()))
+}
+
+func yamlFlowScalarParseFunc() parser.Func {
+	endOfFlowItemRune := func(r rune) bool {
+		return r == ',' || r == '{' || r == '}' || r == '[' || r == ']' || r == '#' || r == '\n'
+	}
+
+	return yamlSkipIndentation(
+		yamlStringParseFunc().
+			Or(yamlMultiWordUnquotedScalarParseFunc(endOfFlowItemRune)).
+			Or(jsonNumberParseFunc()).
+			Or(yamlSingleWordUnquotedScalarParseFunc()))
 }
 
 func yamlCommentParseFunc() parser.Func {
@@ -79,8 +353,40 @@ func yamlCommentParseFunc() parser.Func {
 		Map(recognizeToken(parser.TokenRoleComment))
 }
 
-func yamlKeyParseFunc() parser.Func {
-	return consumeRunesLike(jsonIdentifierRune).
-		Then(jsonConsumeToKeyEndParseFunc()).
-		Map(recognizeToken(yamlTokenRoleKey))
+// yamlFlowStyle represents the current flow style (map or array)
+// "Flow" style in YAML uses brackets and braces to deliminate maps and arrays
+// (similar to JSON). This contrasts with the more common "block" style.
+type yamlFlowStyle byte
+
+const (
+	yamlFlowStyleMap = yamlFlowStyle(iota)
+	yamlFlowStyleArray
+)
+
+// yamlParseState represents the state of the YAML parser.
+// We use this to determine whether we're currently parsing a flow style or block style.
+// To do this, maintain a stack of flow style states. Starting a map or array ("{" or "[")
+// pushes to the stack; ending a map or array ("}" or "]") pops from the stack.
+// If the stack is empty, we're in block style; otherwise, we're in flow style.
+type yamlParseState struct {
+	flowStyleStack []yamlFlowStyle
+}
+
+func (s yamlParseState) Equals(other parser.State) bool {
+	otherState, ok := other.(yamlParseState)
+	if !ok {
+		return false
+	}
+
+	if len(s.flowStyleStack) != len(otherState.flowStyleStack) {
+		return false
+	}
+
+	for i := 0; i < len(s.flowStyleStack); i++ {
+		if s.flowStyleStack[i] != otherState.flowStyleStack[i] {
+			return false
+		}
+	}
+
+	return true
 }
--- syntax/languages/yaml_test.go
+++ syntax/languages/yaml_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestYamlParseFunc(t *testing.T) {
-	const tokenRoleKey = parser.TokenRoleCustom1
 	testCases := []struct {
 		name     string
 		text     string
@@ -36,14 +35,14 @@ func TestYamlParseFunc(t *testing.T) {
 			name: "key without quotes",
 			text: "abc: xyz",
 			expected: []TokenWithText{
-				{Text: `abc:`, Role: tokenRoleKey},
+				{Text: `abc:`, Role: yamlTokenRoleKey},
 			},
 		},
 		{
 			name: "key with single-quoted string",
 			text: "'abc': xyz",
 			expected: []TokenWithText{
-				{Text: `'abc':`, Role: tokenRoleKey},
+				{Text: `'abc':`, Role: yamlTokenRoleKey},
 			},
 		},
 		{
@@ -68,13 +67,21 @@ func TestYamlParseFunc(t *testing.T) {
 			},
 		},
 		{
+			name: "list item with unquoted scalar starting with hyphen",
+			text: `- -s -w -X go test`,
+			expected: []TokenWithText{
+				{Text: `-`, Role: parser.TokenRoleOperator},
+			},
+		},
+		{
 			name: "doc with keys, number, and comment",
 			text: `- foo: # this is a test
 							bar: 123`,
 			expected: []TokenWithText{
-				{Text: `foo:`, Role: tokenRoleKey},
+				{Text: `-`, Role: parser.TokenRoleOperator},
+				{Text: `foo:`, Role: yamlTokenRoleKey},
 				{Text: "# this is a test\n", Role: parser.TokenRoleComment},
-				{Text: `bar:`, Role: tokenRoleKey},
+				{Text: `bar:`, Role: yamlTokenRoleKey},
 				{Text: `123`, Role: parser.TokenRoleNumber},
 			},
 		},
@@ -82,7 +89,155 @@ func TestYamlParseFunc(t *testing.T) {
 			name: "unquoted string with number suffix",
 			text: "foo: v0.1.2",
 			expected: []TokenWithText{
-				{Text: `foo:`, Role: tokenRoleKey},
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+			},
+		},
+		{
+			name: "unquoted scalar after key",
+			text: "foo: 123 abc",
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+			},
+		},
+		{
+			name: "unquoted scalar on next line after key",
+			text: `
+foo:
+  123 abc "xyz" # test
+`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: "# test\n", Role: parser.TokenRoleComment},
+			},
+		},
+		{
+			name: "unquoted scalar containing colon",
+			text: `image: mysql:5.7`,
+			expected: []TokenWithText{
+				{Text: `image:`, Role: yamlTokenRoleKey},
+			},
+		},
+		{
+			name: "block style indicator",
+			text: `
+foo: |
+  test 123
+  "abcxyz" # not a comment
+  notkey: notval
+  end
+bar: 678`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `|`, Role: parser.TokenRoleOperator},
+				{Text: "\n  test 123\n  \"abcxyz\" # not a comment\n  notkey: notval\n  end\n", Role: parser.TokenRoleString},
+				{Text: `bar:`, Role: yamlTokenRoleKey},
+				{Text: `678`, Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "block style indicator with fold style plus",
+			text: `
+foo: |+
+  bar`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `|`, Role: parser.TokenRoleOperator},
+				{Text: `+`, Role: parser.TokenRoleOperator},
+				{Text: "\n  bar", Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "block style indicator with fold style minus",
+			text: `
+foo: |-
+  bar`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `|`, Role: parser.TokenRoleOperator},
+				{Text: `-`, Role: parser.TokenRoleOperator},
+				{Text: "\n  bar", Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "override operator",
+			text: `<<: *def`,
+			expected: []TokenWithText{
+				{Text: `<<:`, Role: parser.TokenRoleOperator},
+				{Text: `*def`, Role: yamlTokenRoleAliasOrAnchor},
+			},
+		},
+		{
+			name: "alias and anchor",
+			text: `
+foo: &ref
+bar: *ref
+`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `&ref`, Role: yamlTokenRoleAliasOrAnchor},
+				{Text: `bar:`, Role: yamlTokenRoleKey},
+				{Text: `*ref`, Role: yamlTokenRoleAliasOrAnchor},
+			},
+		},
+		{
+			name: "flow style map",
+			text: `
+foo: {key1: 123, key2: "value2", key3: abc 123}
+bar: 789
+`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `key1:`, Role: yamlTokenRoleKey},
+				{Text: `123`, Role: parser.TokenRoleNumber},
+				{Text: `key2:`, Role: yamlTokenRoleKey},
+				{Text: `"value2"`, Role: parser.TokenRoleString},
+				{Text: `key3:`, Role: yamlTokenRoleKey},
+				{Text: `bar:`, Role: yamlTokenRoleKey},
+				{Text: `789`, Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "flow style array",
+			text: `[abc 123, 456, "string value"]`,
+			expected: []TokenWithText{
+				{Text: `456`, Role: parser.TokenRoleNumber},
+				{Text: `"string value"`, Role: parser.TokenRoleString},
+			},
+		},
+		{
+			name: "flow style with comments",
+			text: `{
+ foo: bar, # comment
+ baz: bat
+}`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: "# comment\n", Role: parser.TokenRoleComment},
+				{Text: `baz:`, Role: yamlTokenRoleKey},
+			},
+		},
+		{
+			name: "nested flow style",
+			text: `{foo: [bar, 123, {baz: 456}]}`,
+			expected: []TokenWithText{
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `123`, Role: parser.TokenRoleNumber},
+				{Text: `456`, Role: parser.TokenRoleNumber},
+			},
+		},
+		{
+			name: "nested flow style in block style",
+			text: `
+key1: {foo: [bar, 123, {baz: 456}]}
+key2: 789
+`,
+			expected: []TokenWithText{
+				{Text: `key1:`, Role: yamlTokenRoleKey},
+				{Text: `foo:`, Role: yamlTokenRoleKey},
+				{Text: `123`, Role: parser.TokenRoleNumber},
+				{Text: `456`, Role: parser.TokenRoleNumber},
+				{Text: `key2:`, Role: yamlTokenRoleKey},
+				{Text: `789`, Role: parser.TokenRoleNumber},
 			},
 		},
 	}

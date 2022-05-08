3ff6d5cdf30c7a12be758c638d0217096d8e2b8c
Will Daly <will.e.daly@gmail.com>
2022-05-08 11:09:05 -0700

    Replace goreleaser with a shell script
    Go releaser doesn't include debug.ReadBuildInfo in the binaries,
    because it runs `go build main.go` instead of
    `go build github.com/aretex/aretext`.
    
    I think it's possible to get goreleaser to do what I want
    (either using proxy builds or waiting for the "skip main check"
    option to get released), but since we're not really using
    much of the functionality, I decided to replace goreleaser
    with a shell script instead.
    
    This reduces the number of dependencies, ensures that
    ldflags and debug.ReadBuildInfo are set correctly,
    and automates some other steps of the process (checking install docs
    version and creating the git tag).

---
+++ release.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+shopt -s failglob
+
+[ $# -ne 2 ] && { echo "Usage: $0 VERSION NAME"; exit 1; }
+
+RELEASE_VERSION=$1
+RELEASE_NAME=$2
+RELEASE_DIR=$(pwd)/dist
+
+echo "git reset and clean"
+git reset --hard && git clean -xfd
+
+echo "checking docs install version"
+if ! grep "$RELEASE_VERSION" ./docs/install.md;
+then
+    echo "Docs install version does not match release version, aborting";
+    exit 1;
+fi
+
+release_tag=v${RELEASE_VERSION}
+echo "git tag ${release_tag} ${RELEASE_NAME}"
+git tag -s -a "$release_tag" -m "$release_tag $RELEASE_NAME"
+
+build() {
+    goos=$1
+    goarch=$2
+    dir=${RELEASE_DIR}/aretext_${RELEASE_VERSION}_${goos}_${goarch}
+    echo "$dir"
+    mkdir -p "$dir"
+    GOOS=$1 GOARCH=$2 go build \
+        -ldflags="-X 'main.version=${RELEASE_VERSION}'" \
+        -o "$dir/aretext" \
+        github.com/aretext/aretext
+
+    cp LICENSE "$dir/"
+    cp -r docs "$dir/"
+
+    archive_cwd=$(dirname "$dir")
+    archive_src=$(basename "$dir")
+    archive_dst=$RELEASE_DIR/${archive_src}.tar.gz
+    echo "$archive_dst"
+    tar -czf "$archive_dst" -C "$archive_cwd" "$archive_src"
+}
+
+build linux amd64
+build linux arm64
+build linux 386
+build darwin amd64
+build darwin arm64
+
+checksums=aretext_${RELEASE_VERSION}_checksums.txt
+echo "$RELEASE_DIR/$checksums"
+(cd "$RELEASE_DIR" && shasum -a 256 ./*.tar.gz > "$checksums")
+
+signatures=$checksums.sig
+echo "$RELEASE_DIR/$signatures"
+(cd "$RELEASE_DIR" && gpg --output "$signatures" --detach-sig "$checksums")
+
+echo "verifying signature and checksums"
+(cd "$RELEASE_DIR" && gpg --verify "$signatures" "$checksums" && shasum -c "$checksums")
+
+echo "publishing git tag"
+git push origin "$release_tag"

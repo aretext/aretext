a222c2cee6c7ce6c3f0d1b63a91446706993eaf6
Will Daly <will.e.daly@gmail.com>
2023-11-19 07:26:26 -0800

    more hacks to make the graph look reasonable

--- input/engine/render.go
+++ input/engine/render.go
@@ -16,17 +16,41 @@ func Render(sm *StateMachine, eventLabelFunc func(start, end Event) string) stri
 	rankdir=LR;
 `)
 
-	for state, cmdId := range sm.acceptCmd {
-		s := fmt.Sprintf("	node [shape = doublecircle, label=\"%d\"]; %d;\n", cmdId, state)
+	// How many states do we have total?
+	var maxState stateId
+	for s := range sm.transitions {
+		if s > maxState {
+			maxState = s
+		}
+	}
+
+	// Node for each state
+	for state := stateId(0); state <= maxState; state++ {
+		var s string
+		if cmdId, ok := sm.acceptCmd[state]; ok {
+			s = fmt.Sprintf("	node [shape = doublecircle, label=\"%d\"]; %d;\n", cmdId, state)
+		} else {
+			s = fmt.Sprintf("	node [shape = circle, width=0.2, style=filled, label=\"\"]; %d;\n", state)
+		}
 		sb.WriteString(s)
 	}
 
-	sb.WriteString("	node [shape = circle, width=0.2, style=filled, label=\"\"];\n")
+	// huge hack, pseudo-states for each edge
+	pseudoState := 1000000
 	for state, transitions := range sm.transitions {
 		for _, t := range transitions {
 			eventLabel := eventLabelFunc(t.eventRange.start, t.eventRange.end)
-			s := fmt.Sprintf("	%d -> %d [label = \"%s\"];\n", state, t.nextState, eventLabel)
-			sb.WriteString(s)
+
+			// hack: split each edge into two so we can inject the label as an intermediate node.
+			e1 := fmt.Sprintf("  %d -> %d [dir=none];\n", state, pseudoState)
+			s1 := fmt.Sprintf("  node [shape = \"box\", style=\"filled\", fillcolor=\"#E6E6E6\", color=\"#FFFFFF\", label=\"%s\"]; %d;\n", eventLabel, pseudoState)
+			e2 := fmt.Sprintf("	%d -> %d [dir=forward];\n", pseudoState, t.nextState)
+
+			sb.WriteString(s1)
+			sb.WriteString(e1)
+			sb.WriteString(e2)
+
+			pseudoState++
 		}
 	}
 

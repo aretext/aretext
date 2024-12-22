ed7c511d8fd6783c7f72790572b02a5a898ec785
Will Daly <will.e.daly@gmail.com>
2024-12-22 15:03:17 -0800

    Wip

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -2,20 +2,27 @@ package protocol
 
 import (
 	"context"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math"
 	"net"
 	"os"
+	"syscall"
 )
 
 // TODO
-func SendClientHello(ctx context.Context, conn *net.UnixConn, m *ClientHelloMsg, pts *os.File) error {
-	return nil
+func SendClientHello(ctx context.Context, conn *net.UnixConn, msg *ClientHelloMsg, pts *os.File) error {
+	ptsFd := int(pts.Fd())
+	return sendFramedMsgData(ctx, conn, msg, &ptsFd)
 }
 
 func ReceiveClientHello(ctx context.Context, conn *net.UnixConn) (*ClientHelloMsg, *os.File, error) {
-	return nil, nil, nil
+
 }
 
-func SendClientGoodbye(ctx context.Context, conn *net.UnixConn, m *ClientGoodbyeMsg) error {
+func SendClientGoodbye(ctx context.Context, conn *net.UnixConn, msg *ClientGoodbyeMsg) error {
 	return nil
 }
 
@@ -23,7 +30,7 @@ func ReceiveClientGoodbye(ctx context.Context, conn *net.UnixConn) (*ClientGoodb
 	return nil, nil
 }
 
-func SendServerHello(ctx context.Context, conn *net.UnixConn, m *ServerHelloMsg) error {
+func SendServerHello(ctx context.Context, conn *net.UnixConn, msg *ServerHelloMsg) error {
 	return nil
 }
 
@@ -31,7 +38,7 @@ func ReceiveServerHello(ctx context.Context, conn *net.UnixConn) (*ServerHelloMs
 	return nil, nil
 }
 
-func SendServerGoodbye(ctx context.Context, conn *net.UnixConn, m *ServerGoodbyeMsg) error {
+func SendServerGoodbye(ctx context.Context, conn *net.UnixConn, msg *ServerGoodbyeMsg) error {
 	return nil
 }
 
@@ -39,10 +46,44 @@ func ReceiveServerGoodbye(ctx context.Context, conn *net.UnixConn) (*ServerGoodb
 	return nil, nil
 }
 
-func SendTerminalResize(ctx context.Context, conn *net.UnixConn, m *TerminalResizeMsg) error {
+func SendTerminalResize(ctx context.Context, conn *net.UnixConn, msg *TerminalResizeMsg) error {
 	return nil
 }
 
 func ReceiveTerminalResize(ctx context.Context, conn *net.UnixConn) (*TerminalResizeMsg, error) {
 	return nil, nil
+}
+
+func sendFramedMsgData[M any](ctx context.Context, conn *net.UnixConn, msg *M, oobFileDesc *int) error {
+	if msg == nil {
+		return errors.New("Message cannot be nil")
+	}
+
+	encodedMsg, err := json.Marshal(*msg)
+	if err != nil {
+		return fmt.Errorf("json.Marshal: %w", err)
+	}
+
+	if len(encodedMsg) >= math.MaxUint16 {
+		return errors.New("Message too long")
+	}
+
+	data := make([]byte, len(encodedMsg)+4)
+	binary.BigEndian.PutUint16(data[0:], uint16(len(encodedMsg)))
+	copy(data[4:], encodedMsg)
+
+	var oob []byte
+	if oobFileDesc != nil {
+		oob = syscall.UnixRights(*oobFileDesc)
+	}
+
+	_, _, err = conn.WriteMsgUnix(data, oob, nil)
+	if err != nil {
+		return fmt.Errorf("net.WriteMsgUnix: %w", err)
+	}
+
+	return nil
+}
+
+func receiveFramedMsgData[M any](ctx context.Context, conn *net.UnixConn) (*M, *int, error) {
 }

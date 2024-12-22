addb6a8ae67914746a81b0d0b968374edbea3529
Will Daly <will.e.daly@gmail.com>
2024-12-22 15:49:33 -0800

    wip

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,18 +1,20 @@
 package protocol
 
 import (
-	"context"
 	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"math"
 	"net"
+	"os"
 	"syscall"
 )
 
+const maxMsgLen = 102400
+
 // SendMessage sends a message over a Unix socket between a client and a server.
-func SendMessage(ctx context.Context, conn *net.UnixConn, msg Message) error {
+func SendMessage(conn *net.UnixConn, msg Message) error {
 	if msg == nil {
 		return errors.New("Message cannot be nil")
 	}
@@ -32,8 +34,8 @@ func SendMessage(ctx context.Context, conn *net.UnixConn, msg Message) error {
 	}
 
 	data := make([]byte, len(encodedMsg)+4)
-	binary.BigEndian.PutUint16(data[4:], uint16(msgType))
-	binary.BigEndian.PutUint16(data[0:], uint16(len(encodedMsg)))
+	binary.BigEndian.PutUint16(data[0:], uint16(msgType))
+	binary.BigEndian.PutUint16(data[2:], uint16(len(encodedMsg)))
 	copy(data[8:], encodedMsg)
 
 	var oob []byte
@@ -50,7 +52,87 @@ func SendMessage(ctx context.Context, conn *net.UnixConn, msg Message) error {
 }
 
 // ReceiveMessage receives a mesage over a Unix socket.
-func ReceiveMessage(ctx context.Context, conn *net.UnixConn) (Message, error) {
-	// TODO
-	return nil, nil
+func ReceiveMessage(conn *net.UnixConn) (Message, error) {
+	var oob [2]byte
+	var headerData [4]byte
+
+	n, oobn, _, _, err := conn.ReadMsgUnix(headerData, oob)
+	if err != nil {
+		return nil, fmt.Errorf("net.ReadMsgUnix: %w", err)
+	} else if n != 4 {
+		return nil, errors.New("Too few bytes read for message header")
+	}
+
+	msgType := msgType(binary.BigEndian.Uint16(headerData[0:]))
+	msgLen := int(binary.BigEndian.Uint16(headerData[2:]))
+
+	if msgLen > maxMsgLen {
+		return nil, errors.New("Invalid length for message")
+	}
+
+	msgData := make([]byte, msgLen)
+	n, _, _, _, err := conn.ReadMsgUnix(msgData, nil)
+	if err != nil {
+		return nil, fmt.Errorf("net.ReadMsgUnix: %w", err)
+	} else if n != msgLen {
+		return nil, errors.New("Too few bytes read for message data")
+	}
+
+	switch msgType {
+	case clientHelloMsgType:
+		var msg ClientHelloMsg
+		if err := json.Unmarshal(msgData, &msg); err != nil {
+			return nil, fmt.Errorf("json.Unmarshal: %w", err)
+		}
+
+		cmsgs, err := syscall.ParseSocketControlMessage(oob[0:oobn])
+		if err != nil {
+			return nil, fmt.Errorf("syscall.ParseSocketControlMessage: %w", err)
+		}
+
+		fds, err := syscall.ParseUnixRights(&cmsgs[0])
+		if err != nil {
+			return nil, fmt.Errorf("syscall.ParseUnixRights: %w", err)
+		} else if len(fds) != 1 {
+			return nil, errors.New("invalid number of file descriptors received for pty")
+		}
+
+		msg.Pts := os.NewFile(uintptr(fds[0]), "")
+		if fd == nil {
+			return nil, errors.New("invalid file descriptor for pty")
+		}
+
+		return msg, nil
+
+	case clientGoodbyeMsgType:
+		var msg ClientGoodbyeMsg
+		if err := json.Unmarshal(msgData, &msg); err != nil {
+			return nil, fmt.Errorf("json.Unmarshal: %w", err)
+		}
+		return msg, nil
+
+	case serverHelloMsgType:
+		var msg ServerHelloMsg
+		if err := json.Unmarshal(msgData, &msg); err != nil {
+			return nil, fmt.Errorf("json.Unmarshal: %w", err)
+		}
+		return msg, nil
+
+	case serverGoodbyeMsgType:
+		var msg ServerGoodbyeMsg
+		if err := json.Unmarshal(msgData, &msg); err != nil {
+			return nil, fmt.Errorf("json.Unmarshal: %w", err)
+		}
+		return msg, nil
+
+	case terminalResizeMsgType:
+		var msg TerminalResizeMsg
+		if err := json.Unmarshal(msgData, &msg); err != nil {
+			return nil, fmt.Errorf("json.Unmarshal: %w", err)
+		}
+		return msg, nil
+
+	default:
+		return nil, errors.New("Unrecognized message type")
+	}
 }

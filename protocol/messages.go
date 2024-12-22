9d8119dd503fb3d6aa303ad5b107062a21340b24
Will Daly <will.e.daly@gmail.com>
2024-12-22 15:25:37 -0800

    wip

--- protocol/messages.go
+++ protocol/messages.go
@@ -1,16 +1,18 @@
 package protocol
 
+import "os"
+
 // ClientId is a unique identifier for a client assigned by the server on connection.
 type ClientId int
 
 // Message represents a serializable message sent between a client and a server.
 type Message interface{}
 
-// MsgType encodes the type of message.
+// msgType encodes the type of message.
 type msgType int
 
 const (
-	invalidMsgType = MsgType(iota) // Zero value is invalid.
+	invalidMsgType = msgType(iota) // Zero value is invalid.
 	clientHelloMsgType
 	clientGoodbyeMsgType
 	serverHelloMsgType
@@ -36,7 +38,6 @@ func msgTypeForMessage(msg Message) msgType {
 }
 
 // ClientHelloMsg is sent from client to server immediately after opening a connection.
-// Along with this message, the client sends out-of-band SCM_RIGHTS with the file descriptor for the pts.
 type ClientHelloMsg struct {
 	// FilePath is the path to the initial file to open in the editor.
 	// May be empty to create a new untitled document.
@@ -50,25 +51,17 @@ type ClientHelloMsg struct {
 	// The server should use these when interacting with the client's delegated tty
 	// or when executing shell commands on behalf of a client.
 	TerminalEnv []string
-}
 
-func (m ClientHelloMsg) MsgType() MsgType {
-	return ClientHelloMsgType
+	// Pts is a pseudoterminal delegated by the client to the server.
+	// This is sent as out-of-band data SCM_RIGHTS over the Unix socket.
+	Pts *os.File `json:"-"`
 }
-
-var _ Message = (*ClientHelloMsg)(nil)
 
 // ClientGoodbyeMsg is sent from a client to gracefully terminate a connection to the server.
 type ClientGoodbyeMsg struct {
 	// Reason indicates the reason why the client terminated the connection.
 	Reason string
 }
-
-func (m ClientGoodbyeMsg) MsgType() MsgType {
-	return ClientGoodbyeMsg
-}
-
-var _ Message = (*ClientGoodbyeMsg)(nil)
 
 // ServerHelloMsg is sent from a server after receiving ClientHello.
 // This tells the client that it has successfully connected to a server.
@@ -77,23 +70,11 @@ type ServerHelloMsg struct {
 	ClientId ClientId
 }
 
-func (m ServerHelloMsg) MsgType() MsgType {
-	return ServerHelloMsgType
-}
-
-var _ Message = (*ServerHelloMsg)(nil)
-
 // ServerGoodbyeMsg is sent from a server to gracefully terminate a connection to the client.
 type ServerGoodbyeMsg struct {
 	// Reason indicates the reason why the server terminated the connection.
 	Reason string
 }
-
-func (m ServerGoodbyeMsg) MsgType() MsgType {
-	return ServerGoodbyeMsgType
-}
-
-var _ Message = (*ServerGoodbyeMsg)(nil)
 
 // TerminalResizeMsg is sent from a client to the server to signal that the terminal has been resized.
 type TerminalResizeMsg struct {
@@ -103,9 +84,3 @@ type TerminalResizeMsg struct {
 	// Height is the new height of the client's terminal.
 	Height int
 }
-
-func (m TerminalResizeMsg) MsgType() MsgType {
-	return TerminalResizeMsgType
-}
-
-var _ Message = (*TerminalResizeMsg)(nil)
--- protocol/protocol.go
+++ protocol/protocol.go
@@ -8,33 +8,11 @@ import (
 	"fmt"
 	"math"
 	"net"
-	"os"
 	"syscall"
 )
 
-func SendClientHello(ctx context.Context, conn *net.UnixConn, msg *ClientHelloMsg, pts *os.File) error {
-	return sendFramedMsgData(ctx, conn, msg, pts)
-}
-
-func SendClientGoodbye(ctx context.Context, conn *net.UnixConn, msg *ClientGoodbyeMsg) error {
-	return sendFramedMsgData(ctx, conn, msg, nil)
-}
-
-func SendServerHello(ctx context.Context, conn *net.UnixConn, msg *ServerHelloMsg) error {
-	return sendFramedMsgData(ctx, conn, msg, nil)
-}
-
-func SendServerGoodbye(ctx context.Context, conn *net.UnixConn, msg *ServerGoodbyeMsg) error {
-	return sendFramedMsgData(ctx, conn, msg, nil)
-}
-
-func SendTerminalResize(ctx context.Context, conn *net.UnixConn, msg *TerminalResizeMsg) error {
-	return sendFramedMsgData(ctx, conn, msg, nil)
-}
-
-func ReceiveMessage(ctx context.Context, conn *net.UnixConn) (
-
-func sendFramedMsgData[M Message](ctx context.Context, conn *net.UnixConn, msg *M, oobFile *os.File) error {
+// SendMessage sends a message over a Unix socket between a client and a server.
+func SendMessage(ctx context.Context, conn *net.UnixConn, msg Message) error {
 	if msg == nil {
 		return errors.New("Message cannot be nil")
 	}
@@ -44,7 +22,7 @@ func sendFramedMsgData[M Message](ctx context.Context, conn *net.UnixConn, msg *
 		return errors.New("Invalid message type")
 	}
 
-	encodedMsg, err := json.Marshal(*msg)
+	encodedMsg, err := json.Marshal(msg)
 	if err != nil {
 		return fmt.Errorf("json.Marshal: %w", err)
 	}
@@ -54,13 +32,13 @@ func sendFramedMsgData[M Message](ctx context.Context, conn *net.UnixConn, msg *
 	}
 
 	data := make([]byte, len(encodedMsg)+4)
-	binary.BigEndian.PutUint16(data[0:], uint16(len(encodedMsg)))
 	binary.BigEndian.PutUint16(data[4:], uint16(msgType))
+	binary.BigEndian.PutUint16(data[0:], uint16(len(encodedMsg)))
 	copy(data[8:], encodedMsg)
 
 	var oob []byte
-	if oobFile != nil {
-		oob = syscall.UnixRights(int(oobFile.Fd()))
+	if clientHelloMsg, ok := msg.(*ClientHelloMsg); ok {
+		oob = syscall.UnixRights(int(clientHelloMsg.Pts.Fd()))
 	}
 
 	_, _, err = conn.WriteMsgUnix(data, oob, nil)
@@ -71,5 +49,8 @@ func sendFramedMsgData[M Message](ctx context.Context, conn *net.UnixConn, msg *
 	return nil
 }
 
-func receiveFramedMsgData[M any](ctx context.Context, conn *net.UnixConn) (*M, *os.File, error) {
+// ReceiveMessage receives a mesage over a Unix socket.
+func ReceiveMessage(ctx context.Context, conn *net.UnixConn) (Message, error) {
+	// TODO
+	return nil, nil
 }

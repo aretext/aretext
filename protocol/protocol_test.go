55adf6dfe4a34d52b33e4ee18fcae109058da138
Will Daly <will.e.daly@gmail.com>
2024-12-22 16:33:06 -0800

    Wip

--- protocol/protocol_test.go
+++ protocol/protocol_test.go
@@ -2,10 +2,13 @@ package protocol
 
 import (
 	"context"
+	"net"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,15 +24,63 @@ func TestSendAndReceive(t *testing.T) {
 	require.NoError(t, err)
 	defer fakePts.Close()
 
-	messages := []Message{
-		ClientHelloMsg{
-			FilePath:    "/test/file",
-			WorkingDir:  "/test",
-			TerminalEnv: []string{"TERM=tmux"},
-			Pts:         fakePts,
-		},
+	readyChan := make(chan struct{})
+	recvChan := make(chan Message, 1)
+	errChan := make(chan error, 1)
+	go func(ctx context.Context, socketPath string) {
+		serverAddr, err := net.ResolveUnixAddr("unix", socketPath)
+		if err != nil {
+			t.Fatalf("Failed to resolve server address: %v", err)
+		}
+
+		listener, err := net.ListenUnix("unix", serverAddr)
+		if err != nil {
+			t.Fatalf("Failed to create listener: %v", err)
+		}
+		defer listener.Close()
+
+		readyChan <- struct{}{}
+
+		conn, err := listener.AcceptUnix()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		defer conn.Close()
+
+		msg, err := ReceiveMessage(conn)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		recvChan <- msg
+	}(ctx, socketPath)
+
+	select {
+	case <-readyChan:
 	}
 
-	go runServer(ctx, socketPath, messages)
+	clientAddr, err := net.ResolveUnixAddr("unix", socketPath)
+	require.NoError(t, err)
+	clientConn, err := net.DialUnix("unix", nil, clientAddr)
+	require.NoError(t, err)
+	defer clientConn.Close()
 
+	msg := ClientHelloMsg{
+		FilePath:    "/test/file",
+		WorkingDir:  "/test",
+		TerminalEnv: []string{"TERM=tmux"},
+		Pts:         fakePts,
+	}
+	err = SendMessage(clientConn, msg)
+	assert.NoError(t, err)
+
+	select {
+	case err := <-errChan:
+		t.Fatalf("Error sending message: %v", err)
+	case receivedMsg := <-recvChan:
+		assert.Equal(t, msg, receivedMsg)
+	case <-time.After(5 * time.Second):
+		t.Fatal("Timeout waiting for message")
+	}
 }

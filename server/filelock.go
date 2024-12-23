c3f31235635eee56150f8142ac09a24af8f9e026
Will Daly <will.e.daly@gmail.com>
2024-12-23 06:58:33 -0800

    Wip

---
+++ server/filelock.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"syscall"
+)
+
+func acquireLock(lockpath string) (func(), error) {
+	f, err := os.Create(lockpath)
+	if err != nil {
+		return nil, fmt.Errorf("os.Open: %w", err)
+	}
+
+	err = syscall.Flock(int(f.Fd()), int(syscall.LOCK_EX|syscall.LOCK_NB))
+	if err != nil {
+		return nil, &fs.PathError{
+			Op:   "lock",
+			Path: f.Name(),
+			Err:  err,
+		}
+	}
+
+	cleanup := func() {
+		syscall.Flock(int(f.Fd()), int(syscall.LOCK_UN))
+		f.Close()
+	}
+
+	return cleanup, nil
+}
--- server/server.go
+++ server/server.go
@@ -27,3 +27,60 @@ func RunServer(config Config) error {
 		clientId++
 	}
 }
+
+func createListenSocket(socketPath string) (*net.UnixListener, error) {
+	err := syscall.Unlink(socketPath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, fmt.Errorf("syscall.Unlink: %w", err)
+	}
+
+	addr, err := net.ResolveUnixAddr("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("net.ResolveUnixAddr: %w", err)
+	}
+
+	fmt.Printf("listening on %s\n", addr)
+	ul, err := net.ListenUnix("unix", addr)
+	if err != nil {
+		return nil, fmt.Errorf("net.ListenUnix: %w", err)
+	}
+
+	return ul, nil
+}
+
+func handleConnection(uc *net.UnixConn, clientId int) {
+	fmt.Printf("client %d connected\n", clientId)
+
+	// Receive client TTY file descriptor over the Unix socket.
+	tty, err := receiveTTY(uc)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error receiving tty: %s\n", err)
+		return
+	}
+	defer tty.Close()
+
+	fmt.Printf("received tty from client %d\n", clientId)
+
+	// Start a shell subprocess connected to the client's tty.
+	ctx := context.Background()
+	cmd := exec.CommandContext(ctx, "/bin/bash", "--noprofile", "--norc")
+	cmd.Env = []string{fmt.Sprintf("CLIENT_ID=%d", clientId)}
+	cmd.Stdin = tty
+	cmd.Stdout = tty
+	cmd.Stderr = tty
+	// https://github.com/golang/go/issues/29458
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid:  true,
+		Setctty: true,
+		Ctty:    0, // this must be a valid FD in the child process, so choose stdin (fd=0)
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error running cmd: %s\n", err)
+		return
+	}
+
+	fmt.Printf("cmd completed for client %d\n", clientId)
+}
+

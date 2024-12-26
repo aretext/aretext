fde6f83f8eab2a413f72719f0cefbe7baa2fbaa0
Will Daly <will.e.daly@gmail.com>
2024-12-26 13:11:45 -0800

    wip

--- server/server.go
+++ server/server.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"syscall"
 
+	"github.com/gdamore/tcell/v2"
+
 	"github.com/aretext/aretext/protocol"
 )
 
@@ -16,13 +18,13 @@ import (
 // The client sends the server a pseudoterminal (pty), which the server uses
 // for input/output from/to the client's terminal.
 type Server struct {
-	config                      Config
+	config Config
 }
 
 // NewServer creates (but does not start) a new server with the given config.
 func NewServer(config Config) *Server {
 	return &Server{
-		config:                      config,
+		config: config,
 	}
 }
 
@@ -88,52 +90,59 @@ func (s *Server) listenForConnections(ul *net.UnixListener) {
 }
 
 func (s *Server) handleConnection(id sessionId, uc *net.UnixConn) {
-	log.Printf("client connected, sessionId=%d\n", sessionId)
+	log.Printf("client connected, sessionId=%d\n", id)
 	defer uc.Close()
 
 	msg, err := receiveStartSessionMsg(uc)
 	if err != nil {
-		log.Printf("error receiving StartSesssionMsg, sessionId=%d: %s\n", sessionId, err)
+		log.Printf("error receiving StartSesssionMsg, sessionId=%d: %s\n", id, err)
 		return
 	}
 
 	termInfo, err := tcell.LookupTerminfo(msg.TerminalEnv["TERM"])
-
-	tty, err := NewTtyFromPts(msg.Pts)
 	if err != nil {
-		log.Printf("error constructing tty from pts, sessionId=%d\n", sessionId)
+		log.Printf("error looking up terminfo, TERM env = %q, sessionId=%d: %s\n", msg.TerminalEnv["TERM"], id, err)
 		return
 	}
-	defer tty.Close()
 
-
-	// TODO: construct a screen for the session
-	// and defer screen.Fini()
-
-	notifyClientDisconnected := func() {
-		s.clientDisconnectedEventChan <- clientDisconnectedEvent{sessionId: id}
+	clientTty, err := NewTtyFromPts(msg.Pts)
+	if err != nil {
+		log.Printf("error constructing tty from pts, sessionId=%d: %s\n", id, err)
+		return
 	}
+	defer clientTty.Close()
+
+	screen, err := tcell.NewTerminfoScreenFromTtyTerminfo(clientTty, terminInfo)
+	if err != nil {
+		log.Printf("error constructing screen for client, sessionId=%d: %s\n", id, err)
+		return
+	}
+
+	err = screen.Init()
+	if err != nil {
+		log.Printf("error initializing screen for client, sessionId=%d: %s\n", id, err)
+		return
+	}
+	defer screen.Fini()
+
+	resizeTermChan := make(chan struct{}, 1)
 
 	// Process ResizeTerminalMsg from the client.
 	go func(uc *net.UnixConn) {
-		defer notifyClientDisconnected()
 		for {
 			msg, err := protocol.ReceiveMessage(uc)
 			if errors.Is(err, io.EOF) {
-				log.Printf("EOF received on client socket, sessionId=%d: %w\n", sessionId, err)
+				log.Printf("EOF received on client socket, sessionId=%d: %w\n", id, err)
 				return
 			} else if err != nil {
-				log.Printf("error receiving message from client: %s\n", err)
+				log.Printf("error receiving message from client, sessionId=%d: %s\n", id, err)
 				return
 			}
 
 			switch msg := msg.(type) {
 			case *protocol.ResizeTerminalMsg:
-				s.terminalResizedEventChan <- terminalResizedEvent{
-					sessionId: id,
-					width:     msg.Width,
-					height:    msg.Height,
-				}
+				log.Printf("received ResizeTerminalMsg from client, sessionId=%d\n", id)
+				resizeTermChan <- struct{}{}
 			default:
 				log.Printf("unexpected message received from client, sessionId=%d\n", id)
 			}

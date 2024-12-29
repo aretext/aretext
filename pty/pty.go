47ccc0150e4acc2f74468f6b92619fc4966df7ef
Will Daly <will.e.daly@gmail.com>
2024-12-29 12:05:43 -0800

    Wip

--- pty/pty.go
+++ pty/pty.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"sync"
 	"syscall"
+	"time"
 	"unsafe"
 
 	"github.com/gdamore/tcell/v2"
 	"golang.org/x/sys/unix"
+	"golang.org/x/term"
 )
 
 func CreatePtyPair() (ptmx *os.File, pts *os.File, err error) {
@@ -86,8 +90,129 @@ func ProxyTtyToPtmxUntilClosed(ptmx *os.File) {
 	}
 }
 
+// TODO
+type ptsTty struct {
+	pts   *os.File
+	saved *term.State
+	stopQ chan struct{}
+	wg    sync.WaitGroup
+	l     sync.Mutex
+}
+
 // TODO: explain this
 func NewTtyFromPts(pts *os.File) (tcell.Tty, error) {
-	// TODO
-	return nil, errors.New("not implemented")
+	var err error
+	tty := &ptsTty{pts: pts}
+	if !term.IsTerminal(int(pts.Fd())) {
+		return nil, errors.New("not a terminal")
+	}
+	if tty.saved, err = term.GetState(int(pts.Fd())); err != nil {
+		return nil, fmt.Errorf("failed to get state: %w", err)
+	}
+	return tty, nil
 }
+
+func (tty *ptsTty) Read(b []byte) (int, error) {
+	return tty.pts.Read(b)
+}
+
+func (tty *ptsTty) Write(b []byte) (int, error) {
+	return tty.pts.Write(b)
+}
+
+func (tty *ptsTty) Close() error {
+	return tty.pts.Close()
+}
+
+func (tty *ptsTty) Start() error {
+	tty.l.Lock()
+	defer tty.l.Unlock()
+
+	var err error
+	_ = tty.pts.SetReadDeadline(time.Time{})
+	saved, err := term.MakeRaw(int(tty.pts.Fd())) // also sets vMin and vTime
+	if err != nil {
+		return err
+	}
+	tty.saved = saved
+
+	tty.stopQ = make(chan struct{})
+	tty.wg.Add(1)
+	go func(stopQ chan struct{}) {
+		defer tty.wg.Done()
+		for {
+			select {
+			case <-stopQ:
+				return
+			}
+		}
+	}(tty.stopQ)
+
+	return nil
+}
+
+func (tty *ptsTty) Drain() error {
+	_ = tty.pts.SetReadDeadline(time.Now())
+
+	_ = syscall.SetNonblock(int(tty.pts.Fd()), true)
+	tio, err := unix.IoctlGetTermios(int(tty.pts.Fd()), unix.TCGETS)
+	if err != nil {
+		return err
+	}
+	tio.Cc[unix.VMIN] = 0
+	tio.Cc[unix.VTIME] = 0
+	if err = unix.IoctlSetTermios(int(tty.pts.Fd()), unix.TCSETSW, tio); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (tty *ptsTty) Stop() error {
+	tty.l.Lock()
+	if err := term.Restore(int(tty.pts.Fd()), tty.saved); err != nil {
+		tty.l.Unlock()
+		return err
+	}
+	_ = tty.pts.SetReadDeadline(time.Now())
+
+	close(tty.stopQ)
+	tty.l.Unlock()
+
+	tty.wg.Wait()
+
+	return nil
+}
+
+func (tty *ptsTty) WindowSize() (tcell.WindowSize, error) {
+	size := tcell.WindowSize{}
+	ws, err := unix.IoctlGetWinsize(int(tty.pts.Fd()), unix.TIOCGWINSZ)
+	if err != nil {
+		return size, err
+	}
+	w := int(ws.Col)
+	h := int(ws.Row)
+	if w == 0 {
+		w, _ = strconv.Atoi(os.Getenv("COLUMNS"))
+	}
+	if w == 0 {
+		w = 80 // default
+	}
+	if h == 0 {
+		h, _ = strconv.Atoi(os.Getenv("LINES"))
+	}
+	if h == 0 {
+		h = 25 // default
+	}
+	size.Width = w
+	size.Height = h
+	size.PixelWidth = int(ws.Xpixel)
+	size.PixelHeight = int(ws.Ypixel)
+	return size, nil
+}
+
+func (tty *ptsTty) NotifyResize(_ func()) {
+	// Not implemented as pts won't receive SIGWINCH
+}
+
+var _ tcell.Tty = (*ptsTty)(nil)

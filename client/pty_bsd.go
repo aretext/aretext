638054d5fdf64e985dcc3490efaf83527f3a50d4
Will Daly <will.e.daly@gmail.com>
2024-12-30 16:44:12 -0800

    wip

--- client/pty_bsd.go
+++ client/pty_bsd.go
@@ -2,7 +2,43 @@
 
 package client
 
+import (
+	"fmt"
+	"os"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
 func unlockPts(ptmxFd int) error {
 	// BSD doesn't require unlocking pts.
 	return nil
+}
+
+func ptsFileFromPtmx(ptmx *os.File) (*os.File, error) {
+	buf := make([]byte, unix.PathMax)
+
+	// Retrieve name of pts device.
+	_, _, errno := unix.Syscall(unix.SYS_IOCTL, uintptr(ptmx.Fd()), uintptr(unix.TIOCPTYGNAME), uintptr(unsafe.Pointer(&buf[0])))
+	if errno != 0 {
+		return nil, fmt.Errorf("ioctl TIOCPTYGNAME failed: %w", errno)
+	}
+
+	// Convert C-string (NULL terminated) to Go string.
+	n := 0
+	for i, b := range buf {
+		if b == 0 {
+			n = i
+			break
+		}
+	}
+	ptsName := string(buf[:n])
+
+	// Open the pts device.
+	pts, err := os.OpenFile(ptsName, os.O_RDWR, 0)
+	if err != nil {
+		return nil, fmt.Errorf("os.OpenFile failed for pts at %q: %w", ptsName, err)
+	}
+
+	return pts, nil
 }

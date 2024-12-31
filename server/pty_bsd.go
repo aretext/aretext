8503ccd90a42b68919eac3ae479ff7f7c263cc12
Will Daly <will.e.daly@gmail.com>
2024-12-30 21:05:00 -0800

    Reapply "try TIOCDRAIN to fix bsd race cond"
    This reverts commit 8acae9dd47047fb425fb87d1b1f7c945232a4662.

--- server/pty_bsd.go
+++ server/pty_bsd.go
@@ -3,6 +3,7 @@
 package server
 
 import (
+	"fmt"
 	"os"
 	"syscall"
 	"time"
@@ -14,14 +15,10 @@ func drainPty(pts *os.File) error {
 	_ = pts.SetReadDeadline(time.Now())
 
 	_ = syscall.SetNonblock(int(pts.Fd()), true)
-	tio, err := unix.IoctlGetTermios(int(pts.Fd()), unix.TIOCGETA)
+
+	err := unix.IoctlSetInt(int(pts.Fd()), unix.TIOCDRAIN, 0)
 	if err != nil {
-		return err
-	}
-	tio.Cc[unix.VMIN] = 0
-	tio.Cc[unix.VTIME] = 0
-	if err = unix.IoctlSetTermios(int(pts.Fd()), unix.TIOCSETAW, tio); err != nil {
-		return err
+		return fmt.Errorf("ioctl TIOCDRAIN failed: %w", err)
 	}
 	return nil
 }

96478bd3b0f9b0519716bdda03c1cf7a190c32af
Will Daly <will.e.daly@gmail.com>
2025-11-27 18:20:41 -0800

    wip, segment refactored

--- text/segment/segment.go
+++ text/segment/segment.go
@@ -7,7 +7,9 @@ import (
 
 // Segment represents a sequence of runes from a larger text (for example a grapheme cluster).
 type Segment struct {
-	bytes []byte
+	bytes        []byte
+	hasNewline   bool
+	isWhitespace bool
 }
 
 // Empty returns a new, empty segment.
@@ -18,6 +20,8 @@ func Empty() *Segment {
 // Clear removes all runes from the segment.
 func (seg *Segment) Clear() *Segment {
 	seg.bytes = seg.bytes[:0]
+	seg.hasNewline = false
+	seg.isWhitespace = false
 	return seg
 }
 
@@ -26,6 +30,8 @@ func (seg *Segment) Append(r rune) *Segment {
 	var b [utf8.UTFMax]byte
 	n := utf8.EncodeRune(b, r)
 	seg.bytes = append(seg.bytes, b[:n])
+	seg.hasNewline = bool(r == '\n')
+	seg.isWhitespace = unicode.IsSpace(r) && (len(seg.bytes) == 0 || seg.isWhitespcae)
 	return seg
 }
 
@@ -44,31 +50,15 @@ func (seg *Segment) ReverseRunes() *Segment {
 
 // NumRunes returns the number of runes in the segment.
 func (seg *Segment) NumRunes() uint64 {
-	return uint64(len(seg.runes))
-}
-
-// Runes returns the runes contained in the segment.
-// Callers should not modify the returned slice.
-func (seg *Segment) Runes() []rune {
-	return seg.runes
+	return uint64(utf8.RuneCount(seg.bytes))
 }
 
 // HasNewline checks whether a segment contains a line feed rune.
 func (seg *Segment) HasNewline() bool {
-	for i := len(seg.runes) - 1; i >= 0; i-- {
-		if seg.runes[i] == '\n' {
-			return true
-		}
-	}
-	return false
+	return seg.hasNewline
 }
 
 // IsWhitespace checks whether the segment contains only whitespace runes (spaces, tabs, etc).
 func (seg *Segment) IsWhitespace() bool {
-	for _, r := range seg.runes {
-		if !unicode.IsSpace(r) {
-			return false
-		}
-	}
-	return len(seg.runes) > 0
+	return seg.isWhitespace
 }

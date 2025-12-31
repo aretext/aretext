7f0333f832f6e4852783ab1433de220ed981d899
Will Daly <will.e.daly@gmail.com>
2025-12-31 09:49:20 -0800

    Move display/helpers.go into display/buffer.go
    There's only one function left in display/helpers.go
    and it's used only in display/buffer.go.

--- display/buffer.go
+++ display/buffer.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"strconv"
+	"unicode"
 
 	"github.com/gdamore/tcell/v2"
 
@@ -220,5 +221,43 @@ func displayLineNum(lineNumberMode config.LineNumberMode, lineNum uint64, cursor
 		}
 	default:
 		panic("Unrecognized line number mode")
+	}
+}
+
+func drawGraphemeCluster(
+	sr *ScreenRegion,
+	col, row int,
+	gc []rune,
+	gcWidth int,
+	style tcell.Style,
+	showTabs bool,
+	showSpaces bool,
+) {
+	startCol := col
+
+	// Style whitespace (newlines, tabs, etc.) but don't set any runes.
+	// This prevents drawing artifacts with '\r\n' where tcell
+	// sends the combining character ('\n') to the terminal.
+	if unicode.IsSpace(gc[0]) {
+		// Always style at least one cell, even when the gcWidth is zero (carriage returns).
+		// Tabs will usually style multiple cells.
+		for col == startCol || col < startCol+gcWidth {
+			sr.Put(col, row, " ", style)
+			col++
+		}
+
+		// Draw a special character to represent a tab.
+		if gc[0] == '\t' && showTabs {
+			sr.Put(startCol, row, string(tcell.RuneRArrow), style.Dim(true))
+		}
+
+		// Draw a special character to represent a space.
+		if gc[0] == ' ' && showSpaces {
+			sr.Put(startCol, row, string(tcell.RuneBullet), style.Dim(true))
+		}
+	} else {
+		// For everything else, tcell expects the grapheme cluster contents to be
+		// written into the first of the cell(s) occupied by the gc.
+		sr.Put(col, row, string(gc), style)
 	}
 }

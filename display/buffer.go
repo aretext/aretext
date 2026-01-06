aretext/aretext

commit 91c6417e88566dbfe42dae31c0b7f184fdd408f5

Add option to escape non-ASCII unicode
Add a new option to escape non-ASCII unicode with config
option `showUnicode` or menu command "Toggle Show Unicode"

Escaped characters are displayed as unicode codepoints,
grouped in angle brackets by grapheme cluster. By default
the foreground color is set to blue, but the style is
configurable.

display/buffer.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/gdamore/tcell/v2"
 
+	"github.com/aretext/aretext/cellwidth"
 	"github.com/aretext/aretext/config"
 	"github.com/aretext/aretext/selection"
 	"github.com/aretext/aretext/state"
 	"github.com/aretext/aretext/syntax/parser"
+	"github.com/aretext/aretext/text"
 	"github.com/aretext/aretext/text/segment"
 )
 
@@ -26,6 +28,7 @@ func DrawBuffer(screen tcell.Screen, palette *Palette, buffer *state.BufferState
 	pos := viewTextOrigin
 	showTabs := buffer.ShowTabs()
 	showSpaces := buffer.ShowSpaces()
+	showUnicode := buffer.ShowUnicode()
 	lineNumMargin := buffer.LineNumMarginWidth() // Zero if line numbers disabled.
 	lineNumberMode := buffer.LineNumberMode()
 	cursorLine := textTree.LineNumForPosition(cursorPos)
@@ -33,6 +36,7 @@ func DrawBuffer(screen tcell.Screen, palette *Palette, buffer *state.BufferState
 	wrappedLineIter := segment.NewWrappedLineIter(wrapConfig, textTree, pos)
 	wrappedLine := segment.Empty()
 	searchMatch := buffer.SearchMatch()
+	textEscaper := &text.Escaper{}
 
 	sr.HideCursor()
 
@@ -65,9 +69,11 @@ func DrawBuffer(screen tcell.Screen, palette *Palette, buffer *state.BufferState
 			cursorPos,
 			selectedRegion,
 			searchMatch,
-			wrapConfig.WidthFunc,
+			wrapConfig.CellWidthSizer,
 			showTabs,
 			showSpaces,
+			showUnicode,
+			textEscaper,
 		)
 		pos += wrappedLine.NumRunes()
 	}
@@ -101,9 +107,11 @@ func drawLineAndSetCursor(
 	cursorPos uint64,
 	selectedRegion selection.Region,
 	searchMatch *state.SearchMatch,
-	gcWidthFunc segment.GraphemeClusterWidthFunc,
+	cellWidthSizer cellwidth.Sizer,
 	showTabs bool,
 	showSpaces bool,
+	showUnicode bool,
+	textEscaper *text.Escaper,
 ) {
 	startPos := pos
 	gcRunes := []rune{'\x00', '\x00', '\x00', '\x00'}[:0] // Stack-allocate runes for the last grapheme cluster.
@@ -127,7 +135,7 @@ func drawLineAndSetCursor(
 			lastGcWasNewline = (r == '\n')
 			gcRunes = append(gcRunes, r)
 		}
-		gcWidth := gcWidthFunc(gcRunes, totalWidth)
+		gcWidth := cellWidthSizer.GraphemeClusterWidth(gcRunes, totalWidth)
 		totalWidth += gcWidth
 
 		if totalWidth > uint64(maxLineWidth) {
@@ -135,25 +143,13 @@ func drawLineAndSetCursor(
 			return
 		}
 
-		style := tcell.StyleDefault
-		if selectedRegion.ContainsPosition(pos) {
-			style = palette.StyleForSelection()
-		} else if searchMatch.ContainsPosition(pos) {
-			style = palette.StyleForSearchMatch()
-		} else {
-			for len(syntaxTokens) > 0 {
-				token := syntaxTokens[0]
-				if token.StartPos <= pos && token.EndPos > pos {
-					style = palette.StyleForTokenRole(token.Role)
-					break
-				} else if token.StartPos > pos {
-					break
-				}
-				syntaxTokens = syntaxTokens[1:]
-			}
-		}
+		// If "show unicode" enabled and grapheme cluster is non-ASCII unicode,
+		// then escape unicode in this grapheme cluster.
+		// This MUST match the same criteria in cellwidth.
+		escapeUnicode := showUnicode && !(len(gcRunes) == 1 && gcRunes[0] <= 127)
 
-		drawGraphemeCluster(sr, col, row, gcRunes, int(gcWidth), style, showTabs, showSpaces)
+		style := styleForGraphemeCluster(pos, palette, selectedRegion, searchMatch, syntaxTokens, escapeUnicode)
+		drawGraphemeCluster(sr, col, row, gcRunes, int(gcWidth), style, showTabs, showSpaces, escapeUnicode, textEscaper)
 
 		if pos-startPos == uint64(maxLineWidth) {
 			// This occurs when there are maxLineWidth characters followed by a line feed.
@@ -224,6 +220,38 @@ func displayLineNum(lineNumberMode config.LineNumberMode, lineNum uint64, cursor
 	}
 }
 
+func styleForGraphemeCluster(
+	pos uint64,
+	palette *Palette,
+	selectedRegion selection.Region,
+	searchMatch *state.SearchMatch,
+	syntaxTokens []parser.Token,
+	escapeUnicode bool,
+) tcell.Style {
+	if selectedRegion.ContainsPosition(pos) {
+		return palette.StyleForSelection()
+	}
+
+	if searchMatch.ContainsPosition(pos) {
+		return palette.StyleForSearchMatch()
+	}
+
+	if escapeUnicode {
+		return palette.StyleForEscapedUnicode()
+	}
+
+	for len(syntaxTokens) > 0 {
+		token := syntaxTokens[0]
+		if token.StartPos <= pos && token.EndPos > pos {
+			return palette.StyleForTokenRole(token.Role)
+		} else if token.StartPos > pos {
+			break
+		}
+		syntaxTokens = syntaxTokens[1:]
+	}
+	return tcell.StyleDefault
+}
+
 func drawGraphemeCluster(
 	sr *ScreenRegion,
 	col, row int,
@@ -232,6 +260,8 @@ func drawGraphemeCluster(
 	style tcell.Style,
 	showTabs bool,
 	showSpaces bool,
+	escapeUnicode bool,
+	textEscaper *text.Escaper,
 ) {
 	startCol := col
 
@@ -255,6 +285,8 @@ func drawGraphemeCluster(
 		if gc[0] == ' ' && showSpaces {
 			sr.Put(startCol, row, string(tcell.RuneBullet), style.Dim(true))
 		}
+	} else if escapeUnicode {
+		sr.PutStrStyled(col, row, textEscaper.RunesToStr(gc), style)
 	} else {
 		// For everything else, tcell expects the grapheme cluster contents to be
 		// written into the first of the cell(s) occupied by the gc.

app/default-config.yaml
@@ -16,6 +16,7 @@
     tabSize: 4
     showTabs: false
     showSpaces: false
+    showUnicode: false
     showLineNumbers: false
     lineNumberMode: "absolute"
     lineWrap: "character"

cellwidth/cellwidth.go
@@ -2,13 +2,35 @@ package cellwidth
 
 import (
 	"github.com/rivo/uniseg"
+
+	"github.com/aretext/aretext/text"
 )
+
+// Sizer determines the cellwidth of grapheme clusters.
+type Sizer interface {
+	GraphemeClusterWidth(gc []rune, offsetInLine uint64) uint64
+}
+
+type sizer struct {
+	tabSize     uint64
+	showUnicode bool
+	escaper     *text.Escaper
+}
+
+// New constructs a new Sizer with the specified configuration.
+func New(tabSize uint64, showUnicode bool) Sizer {
+	return &sizer{
+		tabSize:     tabSize,
+		showUnicode: showUnicode,
+		escaper:     &text.Escaper{},
+	}
+}
 
 // GraphemeClusterWidth returns the width in cells of a grapheme cluster.
 // It attempts to handle combining characters, emoji, and regional indicators reasonably,
 // but can't be 100% accurate without knowing how the terminal will render the glyphs.
 // Tab width is determined based on the position within the line.
-func GraphemeClusterWidth(gc []rune, offsetInLine uint64, tabSize uint64) uint64 {
+func (s *sizer) GraphemeClusterWidth(gc []rune, offsetInLine uint64) uint64 {
 	if len(gc) == 0 {
 		return 0
 	}
@@ -19,8 +41,14 @@ func GraphemeClusterWidth(gc []rune, offsetInLine uint64, tabSize uint64) uint64
 	// This ensures that characters after the tab "line up" at even multiples
 	// of the tab size.
 	if gc[0] == '\t' {
-		nextTabPosition := ((offsetInLine / tabSize) + 1) * tabSize
+		nextTabPosition := ((offsetInLine / s.tabSize) + 1) * s.tabSize
 		return nextTabPosition - offsetInLine
+	}
+
+	// If displaying non-ascii unicode codepoints, calculate the width for
+	// the unicode sequence (e.g. "<U+1F9D4,U+200D,U+2642,U+FE0F>")
+	if s.showUnicode && !(len(gc) == 1 && gc[0] <= 127) {
+		return uint64(s.escaper.RunesToStrLen(gc))
 	}
 
 	// It is very important that the cell width matches what tcell expects.

cellwidth/cellwidth_test.go
@@ -139,8 +139,26 @@ func TestGraphemeClusterWidth(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			width := GraphemeClusterWidth(tc.gc, tc.offset, 4)
+			sizer := New(4, false)
+			width := sizer.GraphemeClusterWidth(tc.gc, tc.offset)
 			assert.Equal(t, tc.expectedWidth, width)
 		})
 	}
+}
+
+func TestShowUnicode(t *testing.T) {
+	sizer := New(4, false)
+	sizerWithShowUnicode := New(4, true)
+
+	// emoji "woman technologist with light skin tone"
+	gc := []rune{'\U0001f469', '\U0001f3fb', '\u200d', '\U0001f4bb'}
+
+	// Without escaping, this is an emoji with width=2
+	originalWidth := sizer.GraphemeClusterWidth(gc, 0)
+	assert.Equal(t, originalWidth, uint64(2))
+
+	// With escaping, expect the width of "<U+1F469,U+1F3FB,U+200D,U+1F4BB>"
+	escapedWidth := sizerWithShowUnicode.GraphemeClusterWidth(gc, 0)
+	expected := uint64(len("<U+1F469,U+1F3FB,U+200D,U+1F4BB>"))
+	assert.Equal(t, escapedWidth, expected)
 }

config/config.go
@@ -11,6 +11,7 @@ const DefaultTabSize = 4
 const DefaultTabExpand = false
 const DefaultShowTabs = false
 const DefaultShowSpaces = false
+const DefaultShowUnicode = false
 const DefaultAutoIndent = false
 const DefaultShowLineNumbers = false
 const DefaultLineWrap = LineWrapCharacter
@@ -32,6 +33,9 @@ type Config struct {
 
 	// If enabled, display space characters in the document.
 	ShowSpaces bool
+
+	// If enabled, show codepoints for non-ascii unicode.
+	ShowUnicode bool
 
 	// If enabled, indent a new line to match indentation of the previous line.
 	AutoIndent bool
@@ -96,28 +100,29 @@ type MenuCommandConfig struct {
 
 // Names of styles that can be overridden by configuration.
 const (
-	StyleLineNum       = "lineNum"
-	StyleTokenOperator = "tokenOperator"
-	StyleTokenKeyword  = "tokenKeyword"
-	StyleTokenNumber   = "tokenNumber"
-	StyleTokenString   = "tokenString"
-	StyleTokenComment  = "tokenComment"
-	StyleTokenCustom1  = "tokenCustom1"
-	StyleTokenCustom2  = "tokenCustom2"
-	StyleTokenCustom3  = "tokenCustom3"
-	StyleTokenCustom4  = "tokenCustom4"
-	StyleTokenCustom5  = "tokenCustom5"
-	StyleTokenCustom6  = "tokenCustom6"
-	StyleTokenCustom7  = "tokenCustom7"
-	StyleTokenCustom8  = "tokenCustom8"
-	StyleTokenCustom9  = "tokenCustom9"
-	StyleTokenCustom10 = "tokenCustom10"
-	StyleTokenCustom11 = "tokenCustom11"
-	StyleTokenCustom12 = "tokenCustom12"
-	StyleTokenCustom13 = "tokenCustom13"
-	StyleTokenCustom14 = "tokenCustom14"
-	StyleTokenCustom15 = "tokenCustom15"
-	StyleTokenCustom16 = "tokenCustom16"
+	StyleLineNum        = "lineNum"
+	StyleEscapedUnicode = "escapedUnicode"
+	StyleTokenOperator  = "tokenOperator"
+	StyleTokenKeyword   = "tokenKeyword"
+	StyleTokenNumber    = "tokenNumber"
+	StyleTokenString    = "tokenString"
+	StyleTokenComment   = "tokenComment"
+	StyleTokenCustom1   = "tokenCustom1"
+	StyleTokenCustom2   = "tokenCustom2"
+	StyleTokenCustom3   = "tokenCustom3"
+	StyleTokenCustom4   = "tokenCustom4"
+	StyleTokenCustom5   = "tokenCustom5"
+	StyleTokenCustom6   = "tokenCustom6"
+	StyleTokenCustom7   = "tokenCustom7"
+	StyleTokenCustom8   = "tokenCustom8"
+	StyleTokenCustom9   = "tokenCustom9"
+	StyleTokenCustom10  = "tokenCustom10"
+	StyleTokenCustom11  = "tokenCustom11"
+	StyleTokenCustom12  = "tokenCustom12"
+	StyleTokenCustom13  = "tokenCustom13"
+	StyleTokenCustom14  = "tokenCustom14"
+	StyleTokenCustom15  = "tokenCustom15"
+	StyleTokenCustom16  = "tokenCustom16"
 )
 
 // StyleConfig is a configuration for how text should be displayed.
@@ -155,6 +160,7 @@ func ConfigFromUntypedMap(m map[string]any) Config {
 		TabExpand:       boolOrDefault(m, "tabExpand", DefaultTabExpand),
 		ShowTabs:        boolOrDefault(m, "showTabs", DefaultShowTabs),
 		ShowSpaces:      boolOrDefault(m, "showSpaces", DefaultShowSpaces),
+		ShowUnicode:     boolOrDefault(m, "showUnicode", DefaultShowUnicode),
 		AutoIndent:      boolOrDefault(m, "autoIndent", DefaultAutoIndent),
 		ShowLineNumbers: boolOrDefault(m, "showLineNumbers", DefaultShowLineNumbers),
 		LineNumberMode:  stringOrDefault(m, "lineNumberMode", string(DefaultLineNumberMode)),

display/buffer_test.go
@@ -1160,3 +1160,55 @@ func TestShowSpaces(t *testing.T) {
 		})
 	}
 }
+
+func TestShowUnicode(t *testing.T) {
+	testCases := []struct {
+		name             string
+		width, height    int
+		showUnicode      bool
+		inputString      string
+		expectedContents [][]string
+	}{
+		{
+			name:        "render unicode",
+			width:       10,
+			height:      2,
+			showUnicode: false,
+			// emoji "frowning face"
+			inputString: "\u2639",
+			expectedContents: [][]string{
+				{"\u2639", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+				{" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+			},
+		},
+		{
+			name:        "escape unicode",
+			width:       10,
+			height:      2,
+			showUnicode: true,
+			// emoji "frowning face"
+			inputString: "\u2639",
+			expectedContents: [][]string{
+				{"<", "U", "+", "2", "6", "3", "9", ">", " ", " "},
+				{" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			withSimScreen(t, func(s tcell.SimulationScreen) {
+				s.SetSize(tc.width, tc.height)
+				drawBuffer(t, s, func(editorState *state.EditorState) {
+					for _, r := range tc.inputString {
+						state.InsertRune(editorState, r)
+					}
+					if tc.showUnicode {
+						state.ToggleShowUnicode(editorState)
+					}
+				})
+				assertCellContents(t, s, tc.expectedContents)
+			})
+		})
+	}
+}

display/palette.go
@@ -34,6 +34,7 @@ type Palette struct {
 	textFieldBorderStyle      tcell.Style
 	searchPrefixStyle         tcell.Style
 	searchQueryStyle          tcell.Style
+	escapedUnicodeStyle       tcell.Style
 	tokenRoleStyle            map[parser.TokenRole]tcell.Style
 }
 
@@ -62,6 +63,7 @@ func NewPalette() *Palette {
 		textFieldBorderStyle:      s,
 		searchPrefixStyle:         s,
 		searchQueryStyle:          s,
+		escapedUnicodeStyle:       s.Foreground(tcell.ColorBlue),
 		tokenRoleStyle: map[parser.TokenRole]tcell.Style{
 			parser.TokenRoleOperator: s.Foreground(tcell.ColorPurple),
 			parser.TokenRoleKeyword:  s.Foreground(tcell.ColorOlive),
@@ -95,6 +97,8 @@ func NewPaletteFromConfigStyles(styles map[string]config.StyleConfig) *Palette {
 		switch k {
 		case config.StyleLineNum:
 			p.lineNumStyle = s
+		case config.StyleEscapedUnicode:
+			p.escapedUnicodeStyle = s
 		case config.StyleTokenOperator:
 			p.tokenRoleStyle[parser.TokenRoleOperator] = s
 		case config.StyleTokenKeyword:
@@ -233,6 +237,10 @@ func (p *Palette) StyleForSearchPrefix() tcell.Style {
 
 func (p *Palette) StyleForSearchQuery() tcell.Style {
 	return p.searchQueryStyle
+}
+
+func (p *Palette) StyleForEscapedUnicode() tcell.Style {
+	return p.escapedUnicodeStyle
 }
 
 func (p *Palette) StyleForTokenRole(tokenRole parser.TokenRole) tcell.Style {

display/palette_test.go
@@ -12,6 +12,10 @@ import (
 
 func TestPaletteFromConfigStyles(t *testing.T) {
 	configStyles := map[string]config.StyleConfig{
+		config.StyleEscapedUnicode: {
+			Color: "blue",
+			Bold:  true,
+		},
 		config.StyleTokenCustom1: {
 			Color: "black",
 			Bold:  true,
@@ -56,6 +60,7 @@ func TestPaletteFromConfigStyles(t *testing.T) {
 		textFieldBorderStyle:      s,
 		searchPrefixStyle:         s,
 		searchQueryStyle:          s,
+		escapedUnicodeStyle:       s.Foreground(tcell.ColorBlue).Bold(true),
 		tokenRoleStyle: map[parser.TokenRole]tcell.Style{
 			parser.TokenRoleOperator: s.Foreground(tcell.ColorPurple),
 			parser.TokenRoleKeyword:  s.Foreground(tcell.ColorOlive),

commit b4eb7321d54b30a372611f2d91d880082e8bab41

Fix quadratic syntax token search in display code
Commit 91c6417e88566dbfe42dae31c0b7f184fdd408f5 accidentally
caused the display code to search every syntax token in the line
for every grapheme cluster position. Fix it by eliminating tokens
that occur before the current position.

display/buffer.go
@@ -118,6 +118,7 @@ func drawLineAndSetCursor(
 	totalWidth := uint64(0)
 	col := 0
 	var lastGcWasNewline bool
+	var tokenForPos *parser.Token
 
 	if startPos == lineStartPos {
 		drawLineNumIfNecessary(sr, palette, row, lineNum, lineNumMargin, lineNumberMode, cursorLine)
@@ -148,7 +149,8 @@ func drawLineAndSetCursor(
 		// This MUST match the same criteria in cellwidth.
 		escapeUnicode := showUnicode && !(len(gcRunes) == 1 && gcRunes[0] <= 127)
 
-		style := styleForGraphemeCluster(pos, palette, selectedRegion, searchMatch, syntaxTokens, escapeUnicode)
+		tokenForPos, syntaxTokens = consumeSyntaxTokensUntilPos(syntaxTokens, pos)
+		style := styleForGraphemeCluster(pos, palette, selectedRegion, searchMatch, tokenForPos, escapeUnicode)
 		drawGraphemeCluster(sr, col, row, gcRunes, int(gcWidth), style, showTabs, showSpaces, escapeUnicode, textEscaper)
 
 		if pos-startPos == uint64(maxLineWidth) {
@@ -220,12 +222,23 @@ func displayLineNum(lineNumberMode config.LineNumberMode, lineNum uint64, cursor
 	}
 }
 
+func consumeSyntaxTokensUntilPos(syntaxTokens []parser.Token, pos uint64) (*parser.Token, []parser.Token) {
+	for i, token := range syntaxTokens {
+		if token.StartPos <= pos && token.EndPos > pos {
+			return &token, syntaxTokens[i:]
+		} else if token.StartPos > pos {
+			return nil, syntaxTokens[i:]
+		}
+	}
+	return nil, nil
+}
+
 func styleForGraphemeCluster(
 	pos uint64,
 	palette *Palette,
 	selectedRegion selection.Region,
 	searchMatch *state.SearchMatch,
-	syntaxTokens []parser.Token,
+	syntaxToken *parser.Token,
 	escapeUnicode bool,
 ) tcell.Style {
 	if selectedRegion.ContainsPosition(pos) {
@@ -240,15 +253,10 @@ func styleForGraphemeCluster(
 		return palette.StyleForEscapedUnicode()
 	}
 
-	for len(syntaxTokens) > 0 {
-		token := syntaxTokens[0]
-		if token.StartPos <= pos && token.EndPos > pos {
-			return palette.StyleForTokenRole(token.Role)
-		} else if token.StartPos > pos {
-			break
-		}
-		syntaxTokens = syntaxTokens[1:]
+	if syntaxToken != nil {
+		return palette.StyleForTokenRole(syntaxToken.Role)
 	}
+
 	return tcell.StyleDefault
 }
 
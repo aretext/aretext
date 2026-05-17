0f44dbb8a8c58f45f3aaa0d7e3d432a1b7749f21
Will Daly <will.e.daly@gmail.com>
2026-05-17 07:17:30 -0700

    wip

--- syntax/languages/dockerfile.go
+++ syntax/languages/dockerfile.go
@@ -44,12 +44,14 @@ func DockerfileParseFunc() parser.Func {
 
 func dockerfileInstructionParseFunc() parser.Func {
 	isAsciiLetter := func(r rune) bool { return r >= 'A' && r < 'z' }
+	isPrecedingWhitespace := func(r rune) bool { return r == ' ' or r == '\t' }
 	instructions := []string{
 		"add", "arg", "cmd", "copy", "entrypoint", "env", "expose",
 		"from", "healthcheck", "label", "maintainer", "onbuild",
 		"run", "shell", "stopsignal", "user", "volume", "workdir",
 	}
-	return consumeRunesLike(isAsciiLetter).
+	return consumeRunesLike(isPrecedingWhitespace).
+		MaybeBefore(consumeRunesLike(isAsciiLetter)).
 		MapWithInput(recognizeKeywordOrConsume(instructions, false)) // case insensitive
 }
 

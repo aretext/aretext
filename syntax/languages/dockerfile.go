85bb51524bf007d68d1e6b1a797905444330aba0
Will Daly <will.e.daly@gmail.com>
2026-05-17 06:44:27 -0700

    wip

--- syntax/languages/dockerfile.go
+++ syntax/languages/dockerfile.go
@@ -11,6 +11,8 @@ const (
 	dockerfileParseStateInstructionArg
 )
 
+// DockerfileParseFunc returns a parser for a Dockerfile.
+// See https://docs.docker.com/reference/dockerfile
 func DockerfileParseFunc() parser.Func {
 	parseComment := matchState(
 		dockerfileParseStateToplevel,
@@ -23,6 +25,8 @@ func DockerfileParseFunc() parser.Func {
 		dockerfileInstructionParseFunc().
 			Map(setState(dockerfileParseStateInstructionArg)))
 
+	// Transitions back to toplevel state at end of the instruction,
+	// taking into account continuations (\ at end of line).
 	parseInstructionArg := matchState(
 		dockerfileParseStateInstructionArg,
 		dockerfileInstructionArgParseFunc())
@@ -31,4 +35,15 @@ func DockerfileParseFunc() parser.Func {
 		dockerfileParseStateToplevel,
 		parseComment.Or(parseInstruction).Or(parseInstructionArg),
 	)
+}
+
+func dockerfileInstructionParseFunc() parser.Func {
+	instructions := []string{
+		"add", "arg", "cmd", "copy", "entrypoint", "env", "expose",
+		"from", "healthcheck", "label", "maintainer", "onbuild",
+		"run", "shell", "stopsignal", "user", "volume", "workdir"
+	}
+}
+
+func dockerfileInstructionArgParseFunc() parser.Func {
 }

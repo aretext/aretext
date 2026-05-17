4fffc96250e884bc04556c5ab3d2ac1a8692765a
Will Daly <will.e.daly@gmail.com>
2026-05-17 06:34:07 -0700

    Structure

--- syntax/languages/dockerfile.go
+++ syntax/languages/dockerfile.go
@@ -4,9 +4,31 @@ import (
 	"github.com/aretext/aretext/syntax/parser"
 )
 
+type dockerfileParseState uint8
+
+const (
+	dockerfileParseStateToplevel = dockerfileParseState(iota)
+	dockerfileParseStateInstructionArg
+)
+
 func DockerfileParseFunc() parser.Func {
-	// TODO
-	return func(parser.TrackingRuneIter, parser.State) parser.Result {
-		return parser.Result{}
-	}
+	parseComment := matchState(
+		dockerfileParseStateToplevel,
+		consumeString("#").
+			ThenMaybe(consumeTonextLineFeed).
+			Map(recognizeToken(parser.TokenRoleComment)))
+
+	parseInstruction := matchState(
+		dockerfileParseStateToplevel,
+		dockerfileInstructionParseFunc().
+			Map(setState(dockerfileParseStateInstructionArg)))
+
+	parseInstructionArg := matchState(
+		dockerfileParseStateInstructionArg,
+		dockerfileInstructionArgParseFunc())
+
+	return initialState(
+		dockerfileParseStateToplevel,
+		parseComment.Or(parseInstruction).Or(parseInstructionArg),
+	)
 }
